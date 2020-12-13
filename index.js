861682dc510f74d94a2f805153ee3c39946c9a10
OscarSix <26691547+KiloOscarSix@users.noreply.github.com>
2020-12-13 02:02:43 +0000

    v0.02
    Created "command" module
    Created "!ping" comand

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
 const Discord = require("discord.js")
-const config = require("./config.json")
-
 const client = new Discord.Client()
+
+const config = require("./config.json")
+const command = require("./command") 
 
 client.on("ready", () => {
     console.log("College King's Bot is Running")
+
+    command(client, "ping", (message) => {
+        message.channel.send("Pong!")
+    })
 })
+
+// let team1 = message.guild.roles.get(config.team1);
+// let team2 = message.guild.roles.get(config.team2);
+
+client.on('guildMemberAdd', member => {
+    if (Math.floor(Math.random() * 2) == 0) {
+        member.addRole(team1).catch(console.error);
+    } else {
+        member.addRole(team2).catch(console.error);
+    }
+});
+
+
 
 client.login(config.token)
