59ea3524ee657fd416002250fb60bf9be8930b92
OscarSix <26691547+KiloOscarSix@users.noreply.github.com>
2021-01-04 23:49:20 +0000

    v0.22

--- commands/misc/customCommands/update.js
+++ commands/misc/customCommands/update.js
@@ -7,7 +7,7 @@ module.exports = {
             .setTitle("Next College Kings Update")
             .setColor("ff0000")
             .setDescription("**If you are interested in the next update, read below:**")
-            .addField("__Patreon Release ($10)__", "**1st January **", true)
+            .addField("__Patreon Release ($10)__", "**RELEASED**", true)
             .addField("__Public/Steam Release__", "**16th January**", true)
             .setImage("https://media.discordapp.net/attachments/769943204673486858/787791290514538516/CollegeKingsTopBanner.jpg?width=1440&height=360")
             .setThumbnail("https://images-ext-2.discordapp.net/external/QOCCliX2PNqo717REOwxtbvIrxVV2DZ1CRc8Svz3vUs/https/collegekingsgame.com/wp-content/uploads/2020/08/college-kings-wide-white.png?width=1440&height=566")
