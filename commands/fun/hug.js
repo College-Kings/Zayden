0223f7e43f9b5c4100a5e624ac2ba0671a1e323d
OscarSix <26691547+KiloOscarSix@users.noreply.github.com>
2021-04-10 22:41:16 +0100

    Fixed hug command

--- commands/fun/hug.js
+++ commands/fun/hug.js
@@ -1,5 +1,5 @@
 const Discord = require("discord.js")
-const config = require("../../serverConfigs/CKConfig.json")
+const botConfig = require("../../botConfig.json")
 
 module.exports = {
     commands: ["hug"],
@@ -10,13 +10,13 @@ module.exports = {
         if (text) { member = message.mentions.members.first().user.username }
 
         let arrayId = "Global"
-        if (message.author.id in config.huggingImgs) { arrayId = message.author.id }
+        if (message.author.id in botConfig.huggingImgs) { arrayId = message.author.id }
 
-        const imgId = Math.floor(Math.random() * config.huggingImgs[arrayId].length)
+        const imgId = Math.floor(Math.random() * botConfig.huggingImgs[arrayId].length)
 
         const embed = new Discord.MessageEmbed()
-            .setTitle(`${message.author.username} hugs ${member.user.username}`)
-            .setImage(config.huggingImgs[arrayId][imgId])
+            .setTitle(`Sending hugs to ${member}`)
+            .setImage(botConfig.huggingImgs[arrayId][imgId])
             .setColor("FFC0CB")
 
         message.channel.send(embed)
