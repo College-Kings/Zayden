f99f36e91754e0adccaa502a0c2373d36fdd9e83
Jany <62150940+Janybanny@users.noreply.github.com>
2021-04-10 22:03:51 +0200

    Update kiss.js
    Added custom user command functionality to the kiss command part 1

--- commands/fun/kiss.js
+++ commands/fun/kiss.js
@@ -4,15 +4,21 @@ const config = require("../../serverConfigs/CKConfig.json")
 module.exports = {
     commands: ["kiss"],
     expectedArgs: "<user>",
-    minArgs: 1,
+    maxArgs: 1,
     callback: (message, arguments, text) => {
-        const imgId = Math.floor(Math.random() * config.kissingImgs.length)
-        const member = message.mentions.members.first()
+        let member = message.author.username
+        if (text) { member = message.mentions.members.first().user.username }
+
+        let arrayId = "Global"
+        if (message.author.id in config.kissingImgs) { arrayId = message.author.id }
+
+        const imgId = Math.floor(Math.random() * config.kissingImgs[arrayId].length)
+
         const embed = new Discord.MessageEmbed()
             .setTitle(`${message.author.username} kisses ${member.user.username}`)
-            .setImage(config.kissingImgs[imgId])
+            .setImage(config.kissingImgs[arrayId][imgId])
             .setColor("FFC0CB")
-        // console.log(kissingImages[imgId])
+
         message.channel.send(embed)
     },
 }
