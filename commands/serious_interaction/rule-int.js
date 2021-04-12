67b80481363a8c629c9c18483767f806b4382220
Janybanny <jan.pleschberger@gmail.com>
2021-04-13 00:12:47 +0200

    Fixed new rule modifiers to remove clutter from the command and add more power to the config

--- commands/serious_interaction/rule-int.js
+++ commands/serious_interaction/rule-int.js
@@ -9,16 +9,25 @@ module.exports = {
     callback: (message, arguments, text) => {
         const id = arguments[0].toLowerCase();
 
-        if (!config.serverRules[id]) {
+        if (!config.serverRules[id] && !config.hiddenRules[id]) {
             message.reply(`There is no rule with the id ${id}`);
             return
         }
-        
-        const embed = new Discord.MessageEmbed()
-            .setTitle(`Rule ${id}`)
-            .setDescription(`**${id}.** ${config.serverRules[id]}\n\n**Please read the rest of the rules in <#747430712617074718>!**`)
-            .setColor("ff0000")
-            .setThumbnail("https://images-ext-2.discordapp.net/external/QOCCliX2PNqo717REOwxtbvIrxVV2DZ1CRc8Svz3vUs/https/collegekingsgame.com/wp-content/uploads/2020/08/college-kings-wide-white.png?width=1440&height=566")
-        message.reply(embed);
+        if (config.serverRules[id]) {
+            const embed = new Discord.MessageEmbed()
+                .setTitle(`Rule ${id}`)
+                .setDescription(`**${id}.** ${config.serverRules[id]}\n\n**Please read the rest of the rules in <#747430712617074718>!**`)
+                .setColor("ff0000")
+                .setThumbnail("https://images-ext-2.discordapp.net/external/QOCCliX2PNqo717REOwxtbvIrxVV2DZ1CRc8Svz3vUs/https/collegekingsgame.com/wp-content/uploads/2020/08/college-kings-wide-white.png?width=1440&height=566")
+            message.reply(embed);
+        }
+        else {
+            const embed = new Discord.MessageEmbed()
+                .setTitle(`Rule ${id}`)
+                .setDescription(`**${id}.** ${config.hiddenRules[id]}\n\n**Please read the rest of the rules in <#747430712617074718>!**`)
+                .setColor("ff0000")
+                .setThumbnail("https://images-ext-2.discordapp.net/external/QOCCliX2PNqo717REOwxtbvIrxVV2DZ1CRc8Svz3vUs/https/collegekingsgame.com/wp-content/uploads/2020/08/college-kings-wide-white.png?width=1440&height=566")
+            message.reply(embed);
+        }
     },
 }
