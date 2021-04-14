bb4e0b204eaa863692b8a606626c567b976aeb7f
OscarSix <26691547+KiloOscarSix@users.noreply.github.com>
2021-04-14 06:45:50 +0100

    added loop(s) command

---
+++ commands/music/loop.js
@@ -0,0 +1,34 @@
+const fs = require("fs")
+const serverConfig = require("../../serverConfigs/CKConfig.json")
+
+function writeToJson() {
+    fs.writeFile("./serverConfigs/CKConfig.json", JSON.stringify(serverConfig, null, 4), function writeJSON(err) {
+        if (err) { return console.log(err); }
+    });
+}
+
+module.exports = {
+    commands: ["loop"],
+    expectedArgs: "<track/song/queue>",
+    permissionError: "Command is currently in development. Limited to staff use only.",
+    maxArgs: 1,
+    callback: (message, arguments, text) => {
+        if (arguments[0] == "track" || arguments[0] == "song") {
+            serverConfig.loopTrack = !serverConfig.loopTrack || true;
+            serverConfig.loopQueue = !serverConfig.loopQueue || false;
+            writeToJson()
+            message.reply("Now looping **track**")
+            return
+        }
+
+        if (arguments[0] == "queue") {
+            serverConfig.loopQueue = !serverConfig.loopQueue || true;
+            serverConfig.loopTrack = !serverConfig.loopTrack || false;
+            writeToJson()
+            message.reply("Now looping **queue**")
+            return
+        }
+    },
+    permissions: [],
+    requiredRoles: ["Security"],
+}
