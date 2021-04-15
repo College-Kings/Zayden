8ac1429984d2ffe88c67c210a59eb1758bf22ffc
OscarSix <26691547+KiloOscarSix@users.noreply.github.com>
2021-04-15 05:53:35 +0100

    added back command

---
+++ commands/music/back.js
@@ -0,0 +1,9 @@
+const music = require("../../musicFunctions")
+
+module.exports = {
+    commands: ["back", "b", "previous", "prev"],
+    maxArgs: 0,
+    callback: (message, arguments, text) => {
+        music.back()
+    },
+}
