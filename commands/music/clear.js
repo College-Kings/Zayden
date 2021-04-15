9f57133a5fcbb8469311f0b4b328b0cf88386fbd
OscarSix <26691547+KiloOscarSix@users.noreply.github.com>
2021-04-15 05:53:42 +0100

    removed clear lockouts

--- commands/music/clear.js
+++ commands/music/clear.js
@@ -2,11 +2,9 @@ const music = require("../../musicFunctions")
 
 module.exports = {
     commands: ["clear"],
-    permissionError: "Command is currently in development. Limited to staff use only.",
+    maxArgs: 0,
     callback: (message, arguments, text) => {
         music.clear()
         message.channel.send("Queue cleared.")
     },
-    permissions: [],
-    requiredRoles: ["Security"],
 }
