904e0b5e2185d45c202eed4509a8209b9f460ad5
OscarSix <26691547+KiloOscarSix@users.noreply.github.com>
2021-04-15 05:54:56 +0100

    added remove range

---
+++ commands/music/removeRange.js
@@ -0,0 +1,14 @@
+const music = require("../../musicFunctions")
+
+module.exports = {
+    commands: ["removeRange", "rr"],
+    permissionError: "Command is currently in development. Limited to staff use only.",
+    callback: (message, arguments, text) => {
+        start = parseInt(arguments[0])
+        end = parseInt(arguments[1])
+        music.removeRange(start, end)
+        message.channel.send(`Removed ${(end + 1) - start} songs from queue.`)
+    },
+    permissions: [],
+    requiredRoles: [],
+}
