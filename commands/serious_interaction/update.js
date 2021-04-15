bf838293c07be0a7c6bb00b1d21a3f1d9a1ec6fc
OscarSix <26691547+KiloOscarSix@users.noreply.github.com>
2021-04-15 22:48:55 +0100

    updated "update" command

--- commands/serious_interaction/update.js
+++ commands/serious_interaction/update.js
@@ -10,6 +10,14 @@ if (typeof(SteamUpdate) !== "undefined") {
     SteamUpdateMS = SteamUpdate.getTime()
 }
 
+function msToTime(ms) {
+    const days = Math.floor(ms / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((ms % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((ms % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((ms % (1000 * 60)) / (1000));
+    return [days, hours, minutes, seconds]
+}
+
 module.exports = {
     commands: ["update"],
     callback: (message, arguments, text) => {
@@ -22,44 +30,40 @@ module.exports = {
 
         let patreonOutput, steamOutput;
         
-        if (newPatreonUpdate < 0) {
+        if (typeof(PatreonUpdate) == "undefined") {
+            patreonOutput = "No public date set."
+        } else if (newPatreonUpdate < 0) {
             patreonOutput = "RELEASED"
         } else {
             const day = PatreonUpdate.toLocaleString("en-GB", { day:"numeric" })
             const month = PatreonUpdate.toLocaleString("en-GB", { month:"long" })
-
-            const days = Math.floor(newPatreonUpdate / (1000 * 60 * 60 * 24));
-            const hours = Math.floor((newPatreonUpdate % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-            const minutes = Math.floor((newPatreonUpdate % (1000 * 60 * 60)) / (1000 * 60));
-            const seconds = Math.floor((newPatreonUpdate % (1000 * 60)) / (1000));
     
+            const [ days, hours, minutes, seconds ] = msToTime(newPatreonUpdate)
+
             patreonOutput = `${day}th ${month}\n${days}d ${hours}h ${minutes}m ${seconds}s`
         }
 
-        if (typeof(SteamUpdate) === "undefined") {
-            steamOutput = "TBD"
+        if (typeof(SteamUpdate) == "undefined") {
+            steamOutput = "Late May or Early June"
         } else if (newSteamUpdate < 0) {
             steamOutput = "RELEASED"
         } else {
             const day = SteamUpdate.toLocaleString("en-GB", { day:"numeric" })
             const month = SteamUpdate.toLocaleString("en-GB", { month:"long" })
 
-            const days = Math.floor(newSteamUpdate / (1000 * 60 * 60 * 24));
-            const hours = Math.floor((newSteamUpdate % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-            const minutes = Math.floor((newSteamUpdate % (1000 * 60 * 60)) / (1000 * 60));
-            const seconds = Math.floor((newSteamUpdate % (1000 * 60)) / (1000));
+            const [ days, hours, minutes, seconds ] = msToTime(newSteamUpdate)
     
             steamOutput = `${day}th ${month}\n${days}d ${hours}h ${minutes}m ${seconds}s`
         }
 
         const embed = new Discord.MessageEmbed()
-            .setTitle("Next College Kings Update (0.8)")
+            .setTitle("Next College Kings Update (0.9)")
             .setColor("ff0000")
             .setDescription("If you are interested in the next update, read below:")
-            .addField("__Patreon Release ($10)__", patreonOutput, true)
-            .addField("__Steam Release__", steamOutput, true)
+            .addField("__Patreon Release__ ($10)", patreonOutput, true)
+            .addField("__Steam Release__ ($5 - $15)", steamOutput, true)
             .setImage("https://media.discordapp.net/attachments/769943204673486858/787791290514538516/CollegeKingsTopBanner.jpg?width=1440&height=360")
-            .setURL("https://www.youtube.com/watch?v=4Kf-W0M5FwA")
+            .setURL("https://www.youtube.com/watch?v=rIelf_KPybE")
             .setThumbnail("https://images-ext-2.discordapp.net/external/QOCCliX2PNqo717REOwxtbvIrxVV2DZ1CRc8Svz3vUs/https/collegekingsgame.com/wp-content/uploads/2020/08/college-kings-wide-white.png?width=1440&height=566")
             .setFooter("https://www.patreon.com/collegekings")
 
