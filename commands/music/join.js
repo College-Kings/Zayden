85e2503a81780a2047fcfa905ce957c96606f227
OscarSix <26691547+KiloOscarSix@users.noreply.github.com>
2021-04-15 05:53:53 +0100

    removed join lock outs

--- commands/music/join.js
+++ commands/music/join.js
@@ -1,6 +1,5 @@
 module.exports = {
     commands: ["join", "j"],
-    permissionError: "Command is currently in development. Limited to staff use only.",
     maxArgs: 0,
     callback: (message, arguments, text) => {
         if (!message.member.voice.channel) {
@@ -8,11 +7,9 @@ module.exports = {
             return
         }
 
-        if (!message.guild.voiceConnection) {
+        if (!message.guild.voice || !message.guild.voice.channelID) {
             message.member.voice.channel.join().then(connection => message.reply(`Joined ${connection.channel.name}`))
         }
 
     },
-    permissions: [],
-    requiredRoles: ["Security"],
 }
