1747ce1e1ef7ae74cf8fc8d0dd659351b8bbd6c9
OscarSix <26691547+KiloOscarSix@users.noreply.github.com>
2021-04-15 05:55:12 +0100

    removed skip lock outs

--- commands/music/skip.js
+++ commands/music/skip.js
@@ -1,13 +1,11 @@
 const music = require("../../musicFunctions")
-const serverConfig = require("../../serverConfigs/745662812335898806.json")
 
 module.exports = {
     commands: ["skip", "next", "n"],
     permissionError: "Command is currently in development. Limited to staff use only.",
     callback: (message, arguments, text) => {
         music.skip()
-        music.getSongTitle(serverConfig.musicQueue[serverConfig.trackPosition]).then(songTitle => message.channel.send(`Now Playing: ${songTitle}`))
     },
     permissions: [],
-    requiredRoles: ["Security"],
+    requiredRoles: [],
 }
