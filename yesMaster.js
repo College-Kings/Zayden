a8d3e075aa46795993830894ecb29b1a7ec7486b
OscarSix <26691547+KiloOscarSix@users.noreply.github.com>
2021-04-17 10:10:07 +0100

    snazzy come back to "shut up zayden"

--- yesMaster.js
+++ yesMaster.js
@@ -3,5 +3,8 @@ module.exports = (client) => {
         if (message.content.toLowerCase() == "shut up <@!787490197943091211>" && message.author.id == "211486447369322506") {
             message.channel.send("Sorry Master Oscar, will try better next time <:pepeKMS:788514824203141181>")
         }
+        else if (message.content.toLowerCase() == "shut up <@!787490197943091211>") {
+            message.reply("No You.")
+        }
     })
 }
