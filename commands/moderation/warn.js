b8eb3c70eedc4d83549732596f793ebed6b69b35
OscarSix <26691547+KiloOscarSix@users.noreply.github.com>
2021-04-24 18:59:17 +0100

    Offer harsher punishments on repeat offenders

--- commands/moderation/warn.js
+++ commands/moderation/warn.js
@@ -15,18 +15,44 @@ module.exports = {
         if (arguments[1]) { var reason = arguments.slice(1).join(" ") }
         else { var reason = "No Reason Given"}
 
-        const serverMsg = new Discord.MessageEmbed()
-        .setTitle(`User Warned`)
-        .setDescription(`**<@${member.id}> has been warned by <@${message.author.id}>\nReason: ${reason}**`)
-        .setColor("ff0000")
-
+        
         const privateMsg = new Discord.MessageEmbed()
-        .setDescription(`You were warned in ${message.guild.name} for: ${reason}`)
 
-        moderation.addLog(message.guild, member, "warning", message.author, reason)
+        var warnings = moderation.getWarnings(message.guild, member)
 
-        message.channel.send(serverMsg)
-        member.user.send(privateMsg)
+        if (warnings) {
+            const muteMsg = new Discord.MessageEmbed()
+            .setTitle(`${member.user.username} has been warned before:`)
+            for (warning in warnings) {
+                muteMsg.addField(`Case ${warning}`, `**Moderator:** <@${warnings[warning].moderator}>\n**Reason:** ${warnings[warning].reason}\n\n`)
+            }
+            message.channel.send(muteMsg)
+
+            const filter = m => m.author.id === message.author.id
+            message.channel.send("Would you like to increase the warning to a 1 hour mute? \`YES\` / \`NO\`").then(() => {
+                message.channel.awaitMessages(filter, { max: 1, time: 30000, errors: ['time'] })
+                .then(messages => {
+                    const msg = messages.first()
+                    if (msg.content.toUpperCase() == 'YES' || msg.content.toUpperCase() == 'Y') {
+                        const mute = require("./mute")
+                        mute.callback(message, [ `<@${member.id}>`, "1h", reason ], `<@${member.id}> 1h ${reason}`)
+                        return
+                    } else { throw "Warning Sent" }
+                }).catch(messages => {
+                    const serverMsg = new Discord.MessageEmbed()
+                    .setTitle(`User Warned`)
+                    .setDescription(`**<@${member.id}> has been warned by <@${message.author.id}>\nReason: ${reason}**`)
+                    .setColor("ff0000")
+                
+                    privateMsg.setDescription(`You were warned in ${message.guild.name} for: ${reason}`)
+                
+                    moderation.addLog(message.guild, member, "warning", message.author, reason)
+                
+                    message.channel.send(serverMsg)
+                    member.user.send(privateMsg)
+                })
+            })
+        }
     },
     requiredRoles: ["Security"],
 }
