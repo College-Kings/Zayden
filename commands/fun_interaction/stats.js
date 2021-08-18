87b804ad4ab0111ff4a84669a839a30d6e570d50
OscarSix <26691547+KiloOscarSix@users.noreply.github.com>
2021-08-19 00:17:32 +0100

    fixed stats command

--- commands/fun_interaction/stats.js
+++ commands/fun_interaction/stats.js
@@ -1,5 +1,5 @@
 const Discord = require("discord.js")
-const fs = require("fs")
+const commom = require("../../common")
 
 module.exports = {
     commands: ["stats"],
@@ -9,20 +9,14 @@ module.exports = {
         let member = message.mentions.members.first();
         if (!member) { member = message.member; }
 
-        if (!fs.existsSync(`./user_configs/${member.id}.json`)) {
-            console.log("Writing member config")
-            fs.writeFileSync(`./user_configs/${member.id}.json`, JSON.stringify(default_config, null, 4), function writeJSON(err) {
-                if (err) { return console.log(err); }
-            });
-        }
-        member_config = require(`../../user_configs/${member.id}`)
+        commom.user_config_setup();
 
         const embed = new Discord.MessageEmbed()
             .setTitle(`${member.user.username} Stats`)
             .addFields(
                 { name: "Number of Stars", value: member_config["number_of_stars"], inline: true },
                 { name: "Given Stars", value: member_config["given_stars"], inline: true },
-                { name: "Recieved Stars", value: member_config["received_stars"], inline: true },
+                { name: "Received Stars", value: member_config["received_stars"], inline: true },
             )
 
         message.channel.send(embed)
