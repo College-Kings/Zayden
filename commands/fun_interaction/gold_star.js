aa881a0a2cd77cef0a0ed73ed03730f8839133ff
OscarSix <26691547+KiloOscarSix@users.noreply.github.com>
2021-08-18 23:27:19 +0100

    cleaned up code

--- commands/fun_interaction/gold_star.js
+++ commands/fun_interaction/gold_star.js
@@ -1,43 +1,19 @@
 const Discord = require("discord.js")
-const fs = require("fs")
+const common = require("../../common")
 
 module.exports = {
     commands: ["give_star", "gs"],
     expectedArgs: "<user> [text]",
     minArgs: 1,
     callback: (message, arguments, text) => {
-        const default_config = {
-            "number_of_stars": 0,
-            "given_stars": 0,
-            "received_stars": 0
-        }
-
         const member = message.mentions.members.first();
         if (!member) {
             message.reply("No member mentioned.")
             return;
         }
-        
         const author = message.member;
-        const server_config = require(`../../serverConfigs/${message.guild.id}`)
-        let member_config;
-        let author_config;
-
-        if (!fs.existsSync(`./user_configs/${member.id}.json`)) {
-            console.log("Writing member config")
-            fs.writeFileSync(`./user_configs/${member.id}.json`, JSON.stringify(default_config, null, 4), function writeJSON(err) {
-                if (err) { return console.log(err); }
-            });
-        }
-        member_config = require(`../../user_configs/${member.id}`)
-
-        if (!fs.existsSync(`./user_configs/${author.id}.json`)) {
-            console.log("Writing author config")
-            fs.writeFileSync(`./user_configs/${author.id}.json`, JSON.stringify(default_config, null, 4), function writeJSON(err) {
-                if (err) { return console.log(err); }
-            });
-        }
-        author_config = require(`../../user_configs/${author.id}`)
+        
+        common.user_config_setup(message);
 
         if (author_config["number_of_stars"] <= 0 && !author.roles.cache.has(server_config.staffRoles)) {
             message.reply("Unable. You have no gold stars to give.");
@@ -51,12 +27,7 @@ module.exports = {
         member_config["number_of_stars"] += 1;
         member_config["received_stars"] += 1;
 
-        fs.writeFileSync(`./user_configs/${author.id}.json`, JSON.stringify(author_config, null, 4), function writeJSON(err) {
-            if (err) { return console.log(err); }
-        });
-        fs.writeFileSync(`./user_configs/${member.id}.json`, JSON.stringify(member_config, null, 4), function writeJSON(err) {
-            if (err) { return console.log(err); }
-        });
+        common.update_user_configs(message)
 
         const embed = new Discord.MessageEmbed()
         .setTitle(`⭐ NEW GOLDEN STAR ⭐`)
