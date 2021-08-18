b8ac4ecef7a0e8f734944a1327735a0eecbe15be
OscarSix <26691547+KiloOscarSix@users.noreply.github.com>
2021-08-18 23:40:27 +0100

    cleaned up common

--- common.js
+++ common.js
@@ -5,31 +5,32 @@ module.exports = {
         const default_config = {
             "number_of_stars": 0,
             "given_stars": 0,
-            "received_stars": 0
+            "received_stars": 0,
+            "unopen_card_packs": 0,
+            "openned_card_packs": 0,
         }
 
         const member = message.mentions.members.first();      
         const author = message.member;
-        let member_config = `./user_configs/${member.id}`
-        let author_config = `./user_configs/${author.id}`
+        let member_config = `./user_configs/${member.id}.json`
+        let author_config = `./user_configs/${author.id}.json`
 
-        if (!fs.existsSync(`${member_config}.json`)) {
+        if (!fs.existsSync(member_config)) {
             console.log("Writing member config")
-            fs.writeFileSync(`${member_config}.json`, JSON.stringify(default_config, null, 4), function writeJSON(err) {
+            fs.writeFileSync(member_config, JSON.stringify(default_config, null, 4), function writeJSON(err) {
                 if (err) { return console.log(err); }
             });
         }
-        member_config = require(member_config)
 
-        if (!fs.existsSync(`${author_config}.json`)) {
+        if (!fs.existsSync(author_config)) {
             console.log("Writing author config")
-            fs.writeFileSync(`${author_config}.json`, JSON.stringify(default_config, null, 4), function writeJSON(err) {
+            fs.writeFileSync(author_config, JSON.stringify(default_config, null, 4), function writeJSON(err) {
                 if (err) { return console.log(err); }
             });
         }
-        author_config = require(author_config)
-    },
 
+    },
+    
     update_user_configs: function (message) {
         const member = message.mentions.members.first();      
         const author = message.member;
