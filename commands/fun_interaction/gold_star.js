a6f066cf8169a8074d1a7005a58ab41819f0fc93
OscarSix <26691547+KiloOscarSix@users.noreply.github.com>
2021-08-19 00:17:21 +0100

    fixed gs code

--- commands/fun_interaction/gold_star.js
+++ commands/fun_interaction/gold_star.js
@@ -8,16 +8,16 @@ module.exports = {
     callback: (message, arguments, text) => {
         const member = message.mentions.members.first();
         if (!member) {
-            message.reply("No member mentioned.")
+            message.reply("No member mentioned.");
             return;
         }
         const author = message.member;
         
         common.user_config_setup(message);
 
-        member_config = require(`../../user_configs/${member.id}.json`)
-        author_config = require(`../../user_configs/${author.id}.json`)
-        server_config = require(`../../serverConfigs/${message.guild.id}.json`)
+        const member_config = require(`../../user_configs/${member.id}.json`);
+        const author_config = require(`../../user_configs/${author.id}.json`);
+        const server_config = require(`../../serverConfigs/${message.guild.id}.json`);
 
         if (author_config["number_of_stars"] <= 0 && !author.roles.cache.has(server_config.staffRoles)) {
             message.reply("Unable. You have no gold stars to give.");
@@ -31,13 +31,12 @@ module.exports = {
         member_config["number_of_stars"] += 1;
         member_config["received_stars"] += 1;
 
-        common.update_user_configs(message)
+        common.update_configs(message, member_config, author_config);
 
         const embed = new Discord.MessageEmbed()
         .setTitle(`⭐ NEW GOLDEN STAR ⭐`)
-        .setDescription(`<@${member.id}> recieved a golden star from <@${author.id}> for a total of ${member_config.number_of_stars}`)
+        .setDescription(`<@${member.id}> received a golden star from <@${author.id}> for a total of ${member_config["number_of_stars"]}`);
 
-        message.channel.send(embed)
-
+        message.channel.send(embed);
     },
 }
