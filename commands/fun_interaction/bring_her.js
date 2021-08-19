47b75f2c111b84e17ce1be5a1086735119afa59b
OscarSix <26691547+KiloOscarSix@users.noreply.github.com>
2021-08-20 00:18:50 +0100

    added bow command

--- commands/fun_interaction/bring_her.js
+++ commands/fun_interaction/bring_her.js
@@ -6,6 +6,6 @@ module.exports = {
     callback: (message, arguments, text) => {
         if (message.member.id != "211486447369322506") { return; }
 
-        message.channel.send("<@204737300159004672> you're Master needs a foot massage!")
+        message.channel.send("<@204737300159004672> your Master needs a foot massage!")
     }
 }
