c104304ab1c8d7221e21e6cb8c001ef754756408
OscarSix <26691547+KiloOscarSix@users.noreply.github.com>
2021-08-25 02:36:46 +0100

    updated rules and guidelines

--- index.js
+++ index.js
@@ -44,6 +44,9 @@ client.on("ready", async () => {
     reactionRoles.addNormalReactionRole(client);
     reactionRoles.removeNormalReactionRole(client);
 
+    const update_guidelines = require("./selfUpdating/update_guidelines");
+    update_guidelines(client, "879894434538459157")
+
     const updateClubs = require("./selfUpdating/updateClubs")
     updateClubs.customClubs(client, "805765564504473641")
     updateClubs.pledgeRoles(client, "805765564504473641")
@@ -76,6 +79,7 @@ client.on("guildCreate", guild => {
         "patreonUpdate": "",
         "steamUpdate": "",
         "serverRules": {},
+        "server_guidelines": {},
         "hiddenRules": {},
         "masters": []
     }
