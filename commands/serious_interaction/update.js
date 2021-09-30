5c6720d5f0b18c8196f6b434f3ee458468dd24e0
OscarSix <26691547+KiloOscarSix@users.noreply.github.com>
2021-09-30 19:41:24 +0100

    updated config var and added exception for staff role

--- commands/serious_interaction/update.js
+++ commands/serious_interaction/update.js
@@ -11,7 +11,9 @@ function msToTime(ms) {
 module.exports = {
     commands: ["update"],
     callback: (message, arguments, text) => {
-        if (message.channel.id != "776139754408247326") {
+        const serverConfig = require(`../../serverConfigs/${message.guild.id}.json`)
+
+        if (message.channel.id != "776139754408247326" && !message.member.roles.cache.has(serverConfig.staffRoles)) {
             message.reply("You're an idiot. Use <#776139754408247326> for commands.").then(msg => {
                 msg.delete({ timeout: 5000 });
             });
@@ -19,16 +21,15 @@ module.exports = {
             return;
         }
 
-        const config = require(`../../serverConfigs/${message.guild.id}.json`)
         const currentTimeMS = new Date().getTime()
 
         // Patreon update
         let patreonOutput = "";
         let patreonDateMS = 0;
         let patreonUpdateMS = 0;
-        const patreonDate = new Date(config.patreonUpdate)
+        const patreonDate = new Date(serverConfig.patreonUpdate)
 
-        if (config.patreonUpdate == "") {
+        if (serverConfig.patreonUpdate == "") {
             patreonOutput = "No public date set."
         } else {
             patreonDateMS = patreonDate.getTime()
@@ -50,9 +51,9 @@ module.exports = {
         let steamOutput = "";
         let steamDateMS = 0;
         let steamUpdateMS = 0;
-        const steamDate = new Date(config.steamUpdate)
+        const steamDate = new Date(serverConfig.steamUpdate)
 
-        if (config.steamUpdate == "") {
+        if (serverConfig.steamUpdate == "") {
             steamOutput = "No public date set."
         } else {
             steamDateMS = steamDate.getTime()
@@ -75,8 +76,8 @@ module.exports = {
             .setTitle(`Next Update`)
             .setColor("ff0000")
             .setDescription("If you are interested in the next update, read below:")
-            .addField(`__Patreon Release__ (${config.patreon_version})`, patreonOutput, true)
-            .addField(`__Steam Release__ (${config.steam_version})`, steamOutput, true)
+            .addField(`__Patreon Release__ (${serverConfig.patreon_version})`, patreonOutput, true)
+            .addField(`__Steam Release__ (${serverConfig.steam_version})`, steamOutput, true)
             .setImage("https://media.discordapp.net/attachments/769943204673486858/787791290514538516/CollegeKingsTopBanner.jpg?width=1440&height=360")
             .setURL("https://www.patreon.com/collegekings")
             .setThumbnail("https://images-ext-2.discordapp.net/external/QOCCliX2PNqo717REOwxtbvIrxVV2DZ1CRc8Svz3vUs/https/collegekingsgame.com/wp-content/uploads/2020/08/college-kings-wide-white.png?width=1440&height=566")
