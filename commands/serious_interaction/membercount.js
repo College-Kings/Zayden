cee550e0141c784e829d68ed5e06a77bea6f4c1e
OscarSix <26691547+KiloOscarSix@users.noreply.github.com>
2021-12-06 03:34:07 +0000

    reset git

--- commands/serious_interaction/membercount.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    commands: ["membercount"],
-    minArgs: 0,
-    maxArgs: 0,
-    callback: (message, arguments, text) => {
-        message.channel.send(`**${message.guild.memberCount}** total members`)
-    },
-}
--- specialCommands/questionMe.js
+++
@@ -1,8 +0,0 @@
-const blacklist = require("../blacklist");
-
-module.exports = function(message) {
-    if (message.mentions.users.size && message.mentions.users.first().id == message.client.user.id && message.content.slice(-1) == "?" && !blacklist.isBlacklisted(message.author.id)) {
-        if (Math.floor(Math.random() * 2)) {  message.reply("Yes"); }
-        else { message.reply("No"); }
-    }
-}
