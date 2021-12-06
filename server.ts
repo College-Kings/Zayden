81d378a9ec2852162b613d59ffd03eb5d2bcc320
OscarSix <26691547+KiloOscarSix@users.noreply.github.com>
2021-12-06 06:35:12 +0000

    added config to constructor

--- server.ts
+++ server.ts
@@ -3,10 +3,17 @@ import { ReactionRole } from "./reactionRole"
 export class Server {
     id: string;
     reactionRoles: ReactionRole[]
+    disabledCommands: string[] | undefined;
+    moderationRoles: string[] | undefined;
+    channels: Record<string, string> | undefined;
+    idNumber: number | undefined;
+    config: {[key: string]: any} | undefined
 
-    constructor(id: string) {
+
+    constructor(id: string, config=undefined) {
         this.id = id;
         this.reactionRoles = []
+        this.config = config
 
         servers[this.id] = this
     }
