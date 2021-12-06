f30d6e45f190de025ac7181e29fed45af91e19ec
OscarSix <26691547+KiloOscarSix@users.noreply.github.com>
2021-12-06 07:27:40 +0000

    updated `update` command

--- commands/serious_interaction/update.ts
+++ commands/serious_interaction/update.ts
@@ -1,6 +1,7 @@
-const Discord = require("discord.js");
+import Discord from "discord.js"
+import { servers } from "../../server"
 
-function msToTime(ms) {
+function msToTime(ms: number) {
     const days = Math.floor(ms / (1000 * 60 * 60 * 24));
     const hours = Math.floor((ms % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     const minutes = Math.floor((ms % (1000 * 60 * 60)) / (1000 * 60));
@@ -10,12 +11,13 @@ function msToTime(ms) {
 
 module.exports = {
     commands: ["update"],
-    callback: (message, arguments, text) => {
-        const serverConfig = require(`../../server_configs/${message.guild.id}.json`)
+    callback: (message: Discord.Message, args: string[], text: string) => {
+        if (!message.guild || !message.member) { return; }
 
-        if (message.channel.id != "776139754408247326" && !message.member.roles.cache.has(serverConfig.staffRoles)) {
+        const server = servers[message.guild.id]
+        if (message.channel.id != "776139754408247326" && !message.member.roles.cache.has(server.moderationRole)) {
             message.reply("You're an idiot. Use <#776139754408247326> for commands.").then(msg => {
-                msg.delete({ timeout: 5000 });
+                setTimeout(() => msg.delete(), 5000);
             });
             message.delete();
             return;
@@ -27,9 +29,9 @@ module.exports = {
         let patreonOutput = "";
         let patreonDateMS = 0;
         let patreonUpdateMS = 0;
-        const patreonDate = new Date(serverConfig.patreonUpdate)
+        const patreonDate = new Date(server.gameVersions.patreonUpdate)
 
-        if (serverConfig.patreonUpdate == "") {
+        if (server.gameVersions.patreonUpdate == "") {
             patreonOutput = "No public date set."
         } else {
             patreonDateMS = patreonDate.getTime()
@@ -51,9 +53,9 @@ module.exports = {
         let steamOutput = "";
         let steamDateMS = 0;
         let steamUpdateMS = 0;
-        const steamDate = new Date(serverConfig.steamUpdate)
+        const steamDate = new Date(server.gameVersions.steamUpdate)
 
-        if (serverConfig.steamUpdate == "") {
+        if (server.gameVersions.steamUpdate == "") {
             steamOutput = "No public date set."
         } else {
             steamDateMS = steamDate.getTime()
@@ -74,10 +76,10 @@ module.exports = {
         // Send update message
         const embed = new Discord.MessageEmbed()
             .setTitle(`Next Update`)
-            .setColor("ff0000")
+            .setColor("#ff0000")
             .setDescription("If you are interested in the next update, read below:")
-            .addField(`__Patreon Release__ (${serverConfig.patreon_version})`, patreonOutput, true)
-            .addField(`__Steam Release__ (${serverConfig.steam_version})`, steamOutput, true)
+            .addField(`__Patreon Release__ (${server.gameVersions.patreon_version})`, patreonOutput, true)
+            .addField(`__Steam Release__ (${server.gameVersions.steam_version})`, steamOutput, true)
             .setImage("https://media.discordapp.net/attachments/769943204673486858/787791290514538516/CollegeKingsTopBanner.jpg?width=1440&height=360")
             .setURL("https://www.patreon.com/collegekings")
             .setThumbnail("https://images-ext-2.discordapp.net/external/QOCCliX2PNqo717REOwxtbvIrxVV2DZ1CRc8Svz3vUs/https/collegekingsgame.com/wp-content/uploads/2020/08/college-kings-wide-white.png?width=1440&height=566")
