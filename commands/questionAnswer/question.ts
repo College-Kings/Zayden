f36c21788913cdade8db6e270186bcf86948464d
OscarSix <26691547+KiloOscarSix@users.noreply.github.com>
2021-12-10 21:50:49 +0000

    updated `question` and `suggest`

---
+++ commands/questionAnswer/question.ts
@@ -0,0 +1,58 @@
+import Discord from "discord.js"
+
+class Question {
+    text: string;
+    user: Discord.User;
+    questionId: number;
+    messageId: string | undefined;
+    answer: Answer | undefined;
+    
+    constructor(text: string, user: Discord.User) {
+        this.text = text
+        this.user = user
+        this.questionId = questions.length
+        this.messageId = undefined;
+        this.answer = undefined;
+    }
+
+    setMessageId(messageId: string) {
+        this.messageId = messageId
+    }
+
+    setAnswer(text: string, user: Discord.User) {
+        this.answer = new Answer(text, user)
+    }
+}
+
+class Answer {
+    text: string;
+    user: Discord.User;
+
+    constructor(text: string, user: Discord.User) {
+        this.text = text
+        this.user = user
+    }
+}
+
+let questions: Question[] = []
+
+module.exports = {
+    commands: ["question", "ask"],
+    expectedArgs: "<question>",
+    minArgs: 1,
+    callback: (message: Discord.Message, args: string[], text: string) => {
+        if (!message.guild) { return }
+
+        const serverConfig = require(`../../server_configs/${message.guild.id}.json`)
+
+        const question = new Question(text, message.author)
+        questions.push(question)
+
+        const embed = new Discord.MessageEmbed()
+        .addField(`Question id: ${question.questionId}`, question.text)
+
+        const channel = message.guild.channels.cache.get(serverConfig.channels.questionChannel) as Discord.TextChannel
+        channel.send({embeds: [embed]}).then((message: Discord.Message) => { question.setMessageId(message.id) })
+    },
+    questions: questions
+}
---
+++ commands/serious_interaction/suggest.ts
@@ -0,0 +1,24 @@
+import Discord from "discord.js"
+
+module.exports = {
+    commands: ["suggest", "suggestion"],
+    expectedArgs: "<text>",
+    permissionError: "",
+    minArgs: 1,
+    callback: (message: Discord.Message, args: string[], text: string) => {
+        if (!message.guild) { return; }
+
+        const config = require(`../../server_configs/${message.guild.id}.json`);
+
+        const embed = new Discord.MessageEmbed()
+        .setTitle(`From: ${message.author.username}`)
+        .setDescription(text);
+
+        let channel = message.guild.channels.cache.get(config.channels.suggestionChannel) as Discord.TextChannel;
+        channel.send({embeds: [embed]})
+        .then((message: Discord.Message) => {
+            message.react("👍");
+            message.react("👎");
+        })
+    },
+}
