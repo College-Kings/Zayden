951d1ec2bcf334bc796c3199f23bbdd7ecbc5445
OscarSix <26691547+KiloOscarSix@users.noreply.github.com>
2021-12-13 14:00:42 +0000

    updated `spoiler`

--- commands/serious_interaction/spoiler.ts
+++ commands/serious_interaction/spoiler.ts
@@ -1,6 +1,8 @@
+import Discord from "discord.js"
+
 module.exports = {
     commands: ["spoiler", "spoilers"],
-    callback: (message, arguments, text) => {
+    callback: (message: Discord.Message, args: string[], text: string) => {
         message.channel.send(`Please keep all conversations about the new update to <#770621445637799946>
 If you have any bugs or questions please post them in <#747428614500384788> and <#888586464814854194>`)
     },
