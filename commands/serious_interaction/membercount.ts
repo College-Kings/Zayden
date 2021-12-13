ba5a4289de3937d8e6f318c7ca500406e15a4b4d
OscarSix <26691547+KiloOscarSix@users.noreply.github.com>
2021-12-13 13:59:53 +0000

    updated `membercount`

--- commands/serious_interaction/membercount.ts
+++ commands/serious_interaction/membercount.ts
@@ -1,8 +1,12 @@
+import Discord from "discord.js"
+
 module.exports = {
     commands: ["membercount"],
     minArgs: 0,
     maxArgs: 0,
-    callback: (message, arguments, text) => {
+    callback: (message: Discord.Message, args: string[], text: string) => {
+        if (!message.guild) { return; }
+
         message.channel.send(`**${message.guild.memberCount}** total members`)
     },
 }
