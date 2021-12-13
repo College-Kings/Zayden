ff8270b4c5eb09f8484f839353d9f088ec8d5341
OscarSix <26691547+KiloOscarSix@users.noreply.github.com>
2021-12-13 14:19:17 +0000

    updated `goodnight`

--- commands/fun_interaction/goodnight.ts
+++ commands/fun_interaction/goodnight.ts
@@ -1,22 +1,18 @@
-const Discord = require("discord.js")
+import Discord from "discord.js";
 const imgConfig = require("../../configs/image_config.json")
 
 module.exports = {
     commands: ["goodnight", "gn"],
     expectedArgs: "<user>",
     maxArgs: 1,
-    callback: (message, arguments, text) => {
-        let member;
-        try { member = message.mentions.members.first().user.username }
-        catch (error) { member = message.author.username }
+    callback: (message: Discord.Message, args: string[], text: string) => {
+        let member: Discord.GuildMember | undefined;
+        if (message.mentions.members) { member = message.mentions.members.first(); }
+        if (!member) { member = message.member as Discord.GuildMember }
 
+        const imgConfig = require("../../configs/image_config.json")
         let arrayId = "Global";
         if (message.author.id in imgConfig.goodNightImgs) { arrayId = message.author.id }
-        else {
-            try {
-                if (message.mentions.members.first().user.id in imgConfig.fuckingImgs) { arrayId = message.mentions.members.first().user.id }
-            } catch {}
-        }
 
         const imgId = Math.floor(Math.random() * imgConfig.goodNightImgs[arrayId].length)
 
