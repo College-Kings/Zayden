6592c755847d4a70916b9bf34b1d70390d20a750
OscarSix <26691547+KiloOscarSix@users.noreply.github.com>
2021-12-13 12:43:15 +0000

    updated yesMaster

--- special_commands/yesMaster.ts
+++ special_commands/yesMaster.ts
@@ -1,6 +1,7 @@
-module.exports = function(message) {
-    try { var serverConfig = require(`../server_configs/${message.guild.id}.json`) }
-    catch { var serverConfig = require("../server_configs/privateMessage.json") }
+import Discord from "discord.js"
+
+export default function (message: Discord.Message) {
+    if (!message.client.user) { return; }
 
     if (message.content.toLowerCase() == `shut up <@!${message.client.user.id}>` && message.author.id == "211486447369322506") {
         message.channel.send(`Sorry Master ${message.author.username}, will try better next time <:pepeKMS:788514824203141181>`)
