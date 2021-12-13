b3256f6fe2c3adb57a31b4d6db5852d59ce2ae86
OscarSix <26691547+KiloOscarSix@users.noreply.github.com>
2021-12-13 14:00:34 +0000

    updated `save`

--- commands/serious_interaction/save.ts
+++ commands/serious_interaction/save.ts
@@ -1,6 +1,8 @@
+import Discord from "discord.js"
+
 module.exports = {
     commands: ["save", "saves"],
-    callback: (message, arguments, text) => {
+    callback: (message: Discord.Message, args: string[], text: string) => {
         message.channel.send("We do our best to retain save integrety with every update however due to the dynamic nature of game development saves might break. If you experiance a save problem please let us know in <#747428614500384788>\n\nReminder:\nWith the major changes in v0.6 saves before this version will not work. We are sorry for the inconvenience. Use CTRL/TAB to quickly skip through the content you have seen")
     },
 }
