f656bd62e86d289b7ff914e47239acf8b5ae3773
OscarSix <26691547+KiloOscarSix@users.noreply.github.com>
2021-12-13 14:00:13 +0000

    updated `rules-int`

--- commands/serious_interaction/rule-int.ts
+++ commands/serious_interaction/rule-int.ts
@@ -1,14 +1,14 @@
-const Discord = require("discord.js");
+import Discord from "discord.js";
 
 module.exports = {
     commands: "rule",
     expectedArgs: "<number>",
     minArgs: 1,
     cooldown: 10,
-    callback: (message, arguments, text) => {
+    callback: (message: Discord.Message, args: string[], text: string) => {
         const config = require("../../server_configs/745662812335898806.json")
 
-        const id = arguments[0].toLowerCase();
+        const id = args[0].toLowerCase();
 
         if (!config.serverRules[id] && !config.hiddenRules[id]) {
             message.reply(`There is no rule with the id ${id}`);
@@ -18,7 +18,7 @@ module.exports = {
             const embed = new Discord.MessageEmbed()
                 .setTitle(`Rule ${id}`)
                 .setDescription(`**${id}.** ${config.serverRules[id]}\n\n**Please read the rest of the rules in <#747430712617074718>!**`)
-                .setColor("ff0000")
+                .setColor("#ff0000")
                 .setThumbnail("https://images-ext-2.discordapp.net/external/QOCCliX2PNqo717REOwxtbvIrxVV2DZ1CRc8Svz3vUs/https/collegekingsgame.com/wp-content/uploads/2020/08/college-kings-wide-white.png?width=1440&height=566")
             message.reply({embeds: [embed]});
         }
@@ -26,7 +26,7 @@ module.exports = {
             const embed = new Discord.MessageEmbed()
                 .setTitle(`Rule ${id}`)
                 .setDescription(`**${id}.** ${config.hiddenRules[id]}\n\n**Please read the rest of the rules in <#747430712617074718>!**`)
-                .setColor("ff0000")
+                .setColor("#ff0000")
                 .setThumbnail("https://images-ext-2.discordapp.net/external/QOCCliX2PNqo717REOwxtbvIrxVV2DZ1CRc8Svz3vUs/https/collegekingsgame.com/wp-content/uploads/2020/08/college-kings-wide-white.png?width=1440&height=566")
             message.reply({embeds: [embed]});
         }
