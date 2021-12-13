95090826458512d132789b2957d24f8a0b73bd51
OscarSix <26691547+KiloOscarSix@users.noreply.github.com>
2021-12-13 14:19:35 +0000

    updated `newYear`

--- commands/fun_interaction/newYear.ts
+++ commands/fun_interaction/newYear.ts
@@ -1,8 +1,8 @@
+import Discord from "discord.js"
+
 module.exports = {
     commands: ["newyear", "ny"],
-    callback: (message, arguments, text) => {
-        const serverConfig = require(`../../server_configs/${message.guild.id}.json`)
-
+    callback: (message: Discord.Message, args: string[], text: string) => {
         if (message.author.id == "211486447369322506") {
             message.channel.send(`Thank you Master ${message.author.username} for letting me survive to 2021 <:pandahappy:788512955641495592>`)
         } else { message.reply("Happy New Year!") }
