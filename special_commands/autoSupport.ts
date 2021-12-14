103de30081e720d9daec90849f2271c74d6a3391
OscarSix <26691547+KiloOscarSix@users.noreply.github.com>
2021-12-14 07:26:33 +0000

    prevent support role from creating threads

--- special_commands/autoSupport.ts
+++ special_commands/autoSupport.ts
@@ -1,12 +1,18 @@
 import Discord from "discord.js"
+import { servers } from "../server";
 
 module.exports = async function(message: Discord.Message) {
     const guild = message.guild
     if (!guild || message.channel.type !== "GUILD_TEXT" || message.author.id == "787490197943091211" || !message.member) { return; }
 
-    const config = require(`../server_configs/${guild.id}.json`)
-    if (!config.channels.supportChannels || !config.channels.supportChannels.includes(message.channel.id) || message.member.roles.cache.has(config.moderationRole)) { return; }
-    const idNumber = config.idNumber.toLocaleString('en', {minimumIntegerDigits: 4, useGrouping: false})
+    const server = servers[message.guild.id]
+    if (!server.channels.supportChannels ||
+    !server.channels.supportChannels.includes(message.channel.id) ||
+    message.member.roles.cache.has(server.roles.moderationRole) ||
+    message.member.roles.cache.has(server.roles.supportRole)) {
+        return; 
+    }
+    const idNumber = server.idNumber.toLocaleString('en', {minimumIntegerDigits: 4, useGrouping: false})
 
     // Create channel thread and send mentions
     let threadName = `${idNumber} - ${message.content}`
@@ -25,10 +31,10 @@ module.exports = async function(message: Discord.Message) {
     })
 
     // Update json file.
-    config.idNumber += 1
+    server.idNumber += 1
 
     const fs = require("fs")
-    fs.writeFile(`./server_configs/${message.guild.id}.json`, JSON.stringify(config, null, 4), (err: any) => {
+    fs.writeFile(`./server_configs/${message.guild.id}.json`, JSON.stringify(server, null, 4), (err: any) => {
         if (err) { return console.log(err); }
     });
 
