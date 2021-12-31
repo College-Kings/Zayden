14fdfbd990d412ac235a1f8f7673537d00b1c652
OscarSix <26691547+KiloOscarSix@users.noreply.github.com>
2021-12-31 10:07:54 +0000

    fixed auto support

--- special_commands/autoSupport.ts
+++ special_commands/autoSupport.ts
@@ -1,34 +1,39 @@
 import Discord from "discord.js"
 import { servers } from "../server";
 
-module.exports = async function(message: Discord.Message) {
+module.exports = async function (message: Discord.Message) {
     const guild = message.guild
     if (!guild || message.channel.type !== "GUILD_TEXT" || message.author.id == "787490197943091211" || !message.member) { return; }
 
     const server = servers[message.guild.id]
     if (!server.channels.supportChannels ||
-    !server.channels.supportChannels.includes(message.channel.id) ||
-    message.member.roles.cache.has(server.roles.moderationRole) ||
-    message.member.roles.cache.has(server.roles.supportRole)) {
-        return; 
+        !server.channels.supportChannels.includes(message.channel.id) ||
+        message.member.roles.cache.has(server.roles.moderationRole) ||
+        message.member.roles.cache.has(server.roles.supportRole)) {
+        return;
     }
-    const idNumber = server.idNumber.toLocaleString('en', {minimumIntegerDigits: 4, useGrouping: false})
+    const idNumber = server.idNumber.toLocaleString('en', { minimumIntegerDigits: 4, useGrouping: false })
 
     // Create channel thread and send mentions
     let threadName = `${idNumber} - ${message.content}`
-    if (threadName.length > 100) {  threadName = threadName.substring(0, 100) }
+    if (threadName.length > 100) { threadName = threadName.substring(0, 100) }
 
     const thread = await message.channel.threads.create({
         name: threadName,
         autoArchiveDuration: 1440,
     })
 
-    thread.send("<@&913374071239102504>")
-    thread.send({
-        content: `${message.author} wrote:\n${message.content}`,
-        embeds: message.embeds,
-        files: [...message.attachments.values()]
-    })
+    if (message.content.length > 2000) {
+        thread.send(`<@&913374071239102504> ${message.author} Content length over 2000 characters please resend message.`)
+    }
+    else {
+        thread.send(`<@&913374071239102504> ${message.author} wrote:`)
+        thread.send({
+            content: message.content,
+            embeds: message.embeds,
+            files: [...message.attachments.values()]
+        })
+    }
 
     // Update json file.
     server.idNumber += 1
