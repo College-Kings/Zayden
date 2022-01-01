15c7958c15dbf42f5787e27109dc33eaf1059036
OscarSix <26691547+KiloOscarSix@users.noreply.github.com>
2022-01-01 00:17:21 +0000

    updated new year command

--- commands/fun_interaction/newYear.ts
+++ commands/fun_interaction/newYear.ts
@@ -4,7 +4,7 @@ module.exports = {
     commands: ["newyear", "ny"],
     callback: (message: Discord.Message, args: string[], text: string) => {
         if (message.author.id == "211486447369322506") {
-            message.channel.send(`Thank you Master ${message.author.username} for letting me survive to 2021 <:pandahappy:788512955641495592>`)
+            message.channel.send(`Thank you Master ${message.author.username} for letting me survive to 2022 <:pandahappy:788512955641495592>`)
         } else { message.reply("Happy New Year!") }
     },
 }
