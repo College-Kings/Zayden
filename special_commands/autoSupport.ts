c2697df4b26d0f87499b55687abfac47bcff1042
Oscar Six <26691547+kilooscarsix@users.noreply.github.com>
2022-04-25 23:59:54 +0100

    Allow messages with over 2000 characters

--- special_commands/autoSupport.ts
+++ special_commands/autoSupport.ts
@@ -1,65 +1,62 @@
 import Discord from "discord.js";
-import { servers } from "../server";
+import {servers} from "../server";
 
 module.exports = async function (message: Discord.Message) {
     const guild = message.guild
     const messageFiles = [...message.attachments.values()]
 
     if (!guild ||
-        (message.content.length == 0 && messageFiles.length == 0) ||
-        !message.member ||
-        message.channel.type !== "GUILD_TEXT" ||
-        message.author.id == "787490197943091211") { return; }
+        (message.content.length == 0 && messageFiles.length == 0)
+        || !message.member
+        || message.channel.type !== "GUILD_TEXT"
+        || message.author.id == "787490197943091211") {
+        return;
+    }
 
     const server = servers[message.guild.id]
-    if (!server.channels.supportChannels ||
-        !server.channels.supportChannels.includes(message.channel.id) ||
-        message.member.roles.cache.has(server.roles.moderationRole) ||
-        message.member.roles.cache.has(server.roles.supportRole)) {
+    if (!server.channels.supportChannels
+        || !server.channels.supportChannels.includes(message.channel.id)
+        || message.member.roles.cache.has(server.roles.moderationRole)
+        || message.member.roles.cache.has(server.roles.supportRole)) {
         return;
     }
 
     // noinspection TypeScriptValidateJSTypes
-    const idNumber = server.idNumber.toLocaleString('en', { minimumIntegerDigits: 4, useGrouping: false })
+    const idNumber = server.idNumber.toLocaleString('en', {minimumIntegerDigits: 4, useGrouping: false})
 
     // Create channel thread and send mentions
     let threadName = `${idNumber} - ${message.content}`
-    if (threadName.length > 100) { threadName = threadName.substring(0, 100) }
+    if (threadName.length > 100) {
+        threadName = threadName.substring(0, 100)
+    }
 
     const thread = await message.channel.threads.create({
         name: threadName,
-        autoArchiveDuration: 1440,
+        autoArchiveDuration: 'MAX',
     })
 
     thread.send(`<@&913374071239102504> ${message.author} wrote:`)
 
-    if (message.content.length > 2000) {
-        thread.send("Message Error: Content length over 2000 characters please resend message.")
-    }
-    else if (message.content.length > 0) {
+    const threadMessages = message.content.match(/(.|[\r\n]){1,2000}/g);
+
+    if (threadMessages) {
+        threadMessages.forEach(messageContent => {
+            thread.send({
+                content: messageContent,
+                embeds: message.embeds,
+                files: messageFiles
+            })
+        })
+    } else {
         thread.send({
-            content: message.content,
             embeds: message.embeds,
             files: messageFiles
         })
-    }
-    else if (messageFiles.length > 0) {
-        thread.send({
-            embeds: message.embeds,
-            files: messageFiles
-        })
-    }
-    else {
-        thread.send("Message Error: Unable to parse message please resend message.")
     }
 
     // Update json file.
     server.idNumber += 1
-
-    const fs = require("fs")
-    fs.writeFile(`./server_configs/${message.guild.id}.json`, JSON.stringify(server, null, 4), (err: any) => {
-        if (err) { return console.log(err); }
-    });
+    require("../common").updateConfig(guild, server)
 
     await Promise.all([message.delete(), message.channel.bulkDelete(1)])
 }
