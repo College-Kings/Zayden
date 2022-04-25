808f8b2d2b3d3f9bd2660632489fecca78893d61
Oscar Six <26691547+KiloOscarSix@users.noreply.github.com>
2022-04-25 05:00:39 +0100

    updated wisdom.ts

--- commands/fun_interaction/wisdom.ts
+++ commands/fun_interaction/wisdom.ts
@@ -2,8 +2,8 @@ import Discord from "discord.js";
 
 module.exports = {
     commands: ["wisdomoftheday", "wisdom", "w"],
-    callback: (message: Discord.Message, args: string[], text: string) => {
-        const images = require("../../configs/image_config.json").huggingImgs.Global
+    callback: (message: Discord.Message) => {
+        const wisdomImages = require("../../configs/image_config.json").wisdomImages
 
         // Returns 0 - 365
         const now = new Date();
@@ -11,15 +11,26 @@ module.exports = {
         const oneDay = 1000 * 60 * 60 * 24;
         const imageIndex = Math.floor((now.valueOf() - start.valueOf()) / oneDay)
 
-        // Check if index is within bounds of the images
-        if (imageIndex < images.length) { 
+        // Check if index is within bounds of the global images
+        if (imageIndex < wisdomImages.length) {
             const embed = new Discord.MessageEmbed()
                 .setTitle("Today's Wisdom")
-                .setImage(images[imageIndex])
+                .setImage(wisdomImages[imageIndex])
 
             message.channel.send({embeds: [embed]})
-        } else { 
-            message.reply("There is no wisdom for today. #BlameJany")
+        } else {
+            const request = require("request")
+
+            request("https://zenquotes.io/api/today", {json: true}, async (err: any, res: any, body: any) => {
+                if (err) {
+                    return console.log(err);
+                }
+                body = body[0]
+                const messageContent = `> ${body.q}\n${body.a}\n*(ZenQuotes API)*`
+                wisdomImages.push(messageContent)
+                await message.channel.send(messageContent)
+            });
+
         }
 
     },
