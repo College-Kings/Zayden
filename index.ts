ad14714e594f0d73a68cdd7d52ebef59bc1b182f
Oscar Six <26691547+KiloOscarSix@users.noreply.github.com>
2022-04-25 03:49:42 +0100

    fixed depricated option

--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@ import Discord from "discord.js";
 import dotenv from "dotenv";
 import fs from "fs";
 import path from "path";
-import { Server, servers } from "./server";
+import {Server, servers} from "./server";
 
 
 dotenv.config()
@@ -30,7 +30,7 @@ client.on("ready", () => {
     console.log(`Zayden is Running, version: ${botConfig.version}`);
 
     if (client.user) {
-        client.user.setPresence({ activities: [{ name: "College Kings" }], status: "online" })
+        client.user.setPresence({activities: [{name: "College Kings"}], status: "online"})
     }
 
     const loadCommands = require("./commands/load_commands");
@@ -86,21 +86,31 @@ client.on("ready", () => {
             servers[guild.id] = server
 
             fs.writeFile(`./server_configs/${guild.id}.json`, JSON.stringify(server, null, 4), function writeJSON(err) {
-                if (err) { return console.log(err); }
+                if (err) {
+                    return console.log(err);
+                }
             });
         }
 
         // Cache reaction messages
         for (let reactionRole of servers[guild.id].reactionRoles) {
             const channel = client.channels.cache.get(reactionRole.channelId) as Discord.TextChannel
-            if (!channel) { break; }
+            if (!channel) {
+                break;
+            }
             reactionRole.channelId = channel.id;
 
             channel.messages.fetch(reactionRole.messageId)
-                .then((msg: Discord.Message) => { reactionRole.messageId = msg.id })
+                .then((msg: Discord.Message) => {
+                    reactionRole.messageId = msg.id
+                })
 
             guild.roles.fetch(reactionRole.roleId)
-                .then(role => { if (role) { reactionRole.roleId = role.id; } })
+                .then(role => {
+                    if (role) {
+                        reactionRole.roleId = role.id;
+                    }
+                })
         }
     })
 
@@ -130,14 +140,18 @@ client.on("guildCreate", async guild => {
     const server = new Server(guild.id)
 
     fs.writeFile(`./server_configs/${guild.id}.json`, JSON.stringify(server, null, 4), function writeJSON(err) {
-        if (err) { return console.log(err); }
+        if (err) {
+            return console.log(err);
+        }
     });
 })
 
 
 client.on("guildDelete", async guild => {
     fs.unlink(`./server_configs/${guild.id}.json`, (error) => {
-        if (error) { console.log(error) }
+        if (error) {
+            console.log(error)
+        }
     })
 })
 
@@ -163,10 +177,14 @@ client.on("messageReactionAdd", async (reaction, user) => {
     for (const reactionRole of server.reactionRoles) {
         if (reaction.message.id == reactionRole.messageId && reaction.emoji.toString() == reactionRole.emoji && user.id !== "907635513341644861") {
             const member = guild.members.cache.find(member => member.id == user.id)
-            if (!member) { break; }
+            if (!member) {
+                break;
+            }
 
             const role = await guild.roles.fetch(reactionRole.roleId)
-            if (!role) { break; }
+            if (!role) {
+                break;
+            }
 
             member.roles.add(role)
                 .catch((error) => console.log(error))
@@ -185,10 +203,14 @@ client.on("messageReactionRemove", async (reaction, user) => {
     for (const reactionRole of server.reactionRoles) {
         if (reaction.message.id == reactionRole.messageId && reaction.emoji.toString() == reactionRole.emoji && user.id !== "907635513341644861") {
             const member = guild.members.cache.find(member => member.id == user.id)
-            if (!member) { break; }
+            if (!member) {
+                break;
+            }
 
             const role = await guild.roles.fetch(reactionRole.roleId)
-            if (!role) { break; }
+            if (!role) {
+                break;
+            }
 
             member.roles.remove(role)
                 .catch((error) => console.log(error))
@@ -200,6 +222,7 @@ client.on("messageReactionRemove", async (reaction, user) => {
 // Events
 client.on("guildMemberUpdate", (oldMember, newMember) => {
     const server_config = require(`./server_configs/${newMember.guild.id}.json`);
+    const guild = newMember.guild
 
     const patreonRoles: Record<string, number> = {
         '745663316776714370': 1, // Freshman
@@ -220,15 +243,19 @@ client.on("guildMemberUpdate", (oldMember, newMember) => {
         const embed = new Discord.MessageEmbed()
             .setTitle("New Patron")
             .setColor(`${newRole.hexColor}`)
-            .setFooter(newMember.guild.name, newMember.guild.iconURL({ dynamic: true }) as string)
             .setThumbnail("https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/8f5967b9-fc84-45f6-a9c3-3938bfba7232/dbujg26-4865d57d-8dcc-435c-ac6e-0d0590f9de37.png/v1/fill/w_1683,h_475,q_70,strp/patreon_logo_by_laprasking_dbujg26-pre.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOiIsImlzcyI6InVybjphcHA6Iiwib2JqIjpbW3siaGVpZ2h0IjoiPD01NzYiLCJwYXRoIjoiXC9mXC84ZjU5NjdiOS1mYzg0LTQ1ZjYtYTljMy0zOTM4YmZiYTcyMzJcL2RidWpnMjYtNDg2NWQ1N2QtOGRjYy00MzVjLWFjNmUtMGQwNTkwZjlkZTM3LnBuZyIsIndpZHRoIjoiPD0yMDQxIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmltYWdlLm9wZXJhdGlvbnMiXX0.95jfkKc4e-WyhcxKoiDGebItWvxmMPadhqYsh7gIsnQ")
             .addField("User", `<@${newMember.id}>`, true)
             .addField("Amount", `$${patreonRoles[newRole.id]}`, true)
             .setTimestamp();
 
+        const serverIconURL = guild.iconURL({dynamic: true})
+        if (serverIconURL) {
+            embed.setFooter({text: guild.name, iconURL: serverIconURL})
+        }
+
         const channel = client.channels.cache.get(server_config.channels.patreonChannel)
         if (channel && channel.isText()) {
-            channel.send({ embeds: [embed] });
+            channel.send({embeds: [embed]});
         }
 
     }
