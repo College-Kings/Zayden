c9d7347e9b44f4148686260a8061d5cde5fcc5e1
Oscar Six <26691547+KiloOscarSix@users.noreply.github.com>
2022-04-26 01:31:10 +0100

    reformatted

--- commands/reaction roles/rr add.ts
+++ commands/reaction roles/rr add.ts
@@ -5,28 +5,36 @@ module.exports = {
     expectedArgs: "<channel> <message> <role> <emoji>",
     minArgs: 4,
     maxArgs: 4,
-    callback: async (message: Discord.Message, args: string[], text: string) => {
+    callback: async (message: Discord.Message, args: string[]) => {
         const guild: Discord.Guild | null = message.guild;
-        if (!guild) { return; }
+        if (!guild) {
+            return;
+        }
 
         // Handle Channel
         const common = require("../../common")
         const channelId: string = common.parseId(args[0]);
         const channel: Discord.Channel | null = await guild.channels.fetch(channelId);
 
-        if (!channel || !channel.isText()) { return message.reply("Invald channel."); }
+        if (!channel || !channel.isText()) {
+            return message.reply("Invald channel.");
+        }
 
         // Handle Message
-        const messageId: string= common.parseId(args[1]);
+        const messageId: string = common.parseId(args[1]);
         const msg: Discord.Message = await channel.messages.fetch(messageId)
 
-        if (!msg) { return message.reply("Invalid message."); }
+        if (!msg) {
+            return message.reply("Invalid message.");
+        }
 
         // Handle Role
         const roleId: string = common.parseId(args[2]);
         const role: Discord.Role | null = await guild.roles.fetch(roleId);
 
-        if (!role) { return message.reply("Invald Role."); }
+        if (!role) {
+            return message.reply("Invald Role.");
+        }
 
         // Handle Emoji
         const emoji = args[3];
@@ -34,9 +42,11 @@ module.exports = {
         // Create ReactionRole
         const status: boolean = require("./functions").addNormalReaction(guild, channel, msg, role, emoji)
 
-        if (!status) { return message.reply("Failed to added reaction") }
+        if (!status) {
+            return message.reply("Failed to added reaction")
+        }
 
-        message.reply("Successfully added reaction")
+        await message.reply("Successfully added reaction")
     },
     permissions: ["MANAGE_ROLES"]
 }
