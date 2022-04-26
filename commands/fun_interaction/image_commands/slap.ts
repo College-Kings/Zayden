fe573321eefcd513a9412b80370630f740ffef4a
Oscar Six <26691547+KiloOscarSix@users.noreply.github.com>
2022-04-26 01:31:03 +0100

    fixed slap command

--- commands/fun_interaction/image_commands/slap.ts
+++ commands/fun_interaction/image_commands/slap.ts
@@ -4,9 +4,14 @@ module.exports = {
     commands: ["slap"],
     expectedArgs: "<user>",
     maxArgs: 1,
-    callback: (message: Discord.Message) => {
+    callback: async (message: Discord.Message) => {
         const member = message.mentions.members?.first() || message.member
         if (!member) {
+            return;
+        }
+
+        if (member.id == "211486447369322506") {
+            await message.reply(`Sorry ${message.author} you cannot slap Master Six`)
             return;
         }
 
@@ -18,9 +23,22 @@ module.exports = {
 
         const imgId = Math.floor(Math.random() * imageConfig.slapImages[arrayId].length)
 
-        const embed = new Discord.MessageEmbed()
+        let embed;
+        if (message.member?.id == "393046490966130688" && member.id == "211486447369322506") { // Sondwich
+            embed = new Discord.MessageEmbed()
+                .setTitle("Oscar slaps Sondwich")
+                .setImage(imageConfig.slapImages[arrayId][imgId]);
+            try {
+                await message.reply({content: "Bitch! You don't slap me.", embeds: [embed]})
+            } catch {
+                message.channel.send({content: "Bitch! You don't slap me.", embeds: [embed]})
+            }
+            return;
+        }
+
+        embed = new Discord.MessageEmbed()
             .setTitle(`${message.author.username} slaps ${member.displayName}`)
-            .setImage(imageConfig.slapImages[arrayId][imgId])
+            .setImage(imageConfig.slapImages[arrayId][imgId]);
 
         message.channel.send({embeds: [embed]})
     },
