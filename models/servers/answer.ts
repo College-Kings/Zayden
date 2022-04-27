d66c6ed78fcfd97fbf103bddcbf6107b7d10ebd3
Oscar Six <26691547+KiloOscarSix@users.noreply.github.com>
2022-04-27 10:46:16 +0100

    deleted unused schemas

--- models/servers/answer.ts
+++
@@ -1,6 +0,0 @@
-import mongoose from "mongoose";
-
-export const AnswerSchema = new mongoose.Schema({
-    text: String,
-    userId: String,
-})
--- models/servers/reaction_role.ts
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-
-export const ReactionRoleSchema = new mongoose.Schema({
-    channelId: String,
-    messageId: String,
-    roleId: String,
-    emoji: String
-})
