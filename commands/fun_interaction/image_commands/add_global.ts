7dfb7b0953424159c5c44a3f492c5df1cc3e8b78
Oscar Six <26691547+KiloOscarSix@users.noreply.github.com>
2022-04-27 09:35:02 +0100

    Updated adding images to database

--- commands/fun_interaction/image_commands/add_global.ts
+++ commands/fun_interaction/image_commands/add_global.ts
@@ -1,31 +1,33 @@
 import Discord from "discord.js"
+import {ImageConfig} from "../../../models/images/imageConfigSchema";
 
 module.exports = {
     commands: ["add_global"],
     expectedArgs: "<category>, <image_link>",
     minArgs: 2,
     maxArgs: 2,
-    callback: async (message: Discord.Message, args: string[]) => {
+    callback: async (message: Discord.Message, server: any, args: string[]) => {
         if (message.author.id != "211486447369322506") {
             return;
         }
 
-        const imageConfig = require("../../../configs/image_config.json")
-        const category = args[0]
-        const image_link = args[1]
+        const imageLink = args[1]
 
-        if (!(category in imageConfig)) {
+        const imageConfig = await ImageConfig.findOne({category: args[0]}).exec()
+
+        if (!imageConfig) {
             await message.reply("Category not found in image config.")
             return;
         }
 
-        imageConfig[category].global.push(image_link)
+        const globalImages = new Set(imageConfig.global)
+        globalImages.add(imageLink)
 
-        const fs = require("fs")
-        fs.writeFile(`./configs/image_config.json`, JSON.stringify(imageConfig, null, 4), (error: any) => {
-            if (error) {
-                return console.log(error);
-            }
-        });
+        imageConfig.global = [...globalImages]
+
+        await Promise.all([
+            imageConfig.save(),
+            message.reply(`Successfully added "${imageLink}" to ${imageConfig.category} images`)
+        ])
     },
 }
--- commands/fun_interaction/image_commands/add_personal.ts
+++ commands/fun_interaction/image_commands/add_personal.ts
@@ -1,40 +1,39 @@
 import Discord from "discord.js"
+import {ImageConfig} from "../../../models/images/imageConfigSchema";
 
 module.exports = {
     commands: ["add_personal"],
-    expectedArgs: "<category>, <member>, <image_link>",
-    minArgs: 3,
-    maxArgs: 3,
-    callback: async (message: Discord.Message, args: string[]) => {
-        if (message.author.id != "211486447369322506") {
+    expectedArgs: "<category>, <image_link>",
+    minArgs: 2,
+    maxArgs: 2,
+    callback: async (message: Discord.Message, server: any, args: string[]) => {
+        const userId = message.author.id
+
+        if (userId != "211486447369322506") {
             return;
         }
 
-        const imageConfig = require("../../../configs/image_config.json")
-        const category = args[0]
-        const memberId = require("../../../common").parseId(args[1])
-        const imageLink = args[2]
+        const imageLink = args[1]
 
-        if (!(category in imageConfig)) {
+        const imageConfig = await ImageConfig.findOne({category: args[0]}).exec()
+
+        if (!imageConfig) {
             await message.reply("Category not found in image config.")
             return;
         }
 
-        if (memberId in imageConfig[category]) {
-            imageConfig[category][memberId].push(imageLink)
-        } else {
-            imageConfig[category][memberId] = [imageLink]
-        }
+        const globalImages = new Set(imageConfig.global)
+        const userImages = new Set(imageConfig.users[userId])
 
-        require("../../../init").updateImages()
+        userImages.add(imageLink)
+        globalImages.add(imageLink)
 
-        const fs = require("fs")
-        fs.writeFile(`./configs/image_config.json`, JSON.stringify(imageConfig, null, 4), (error: any) => {
-            if (error) {
-                return console.log(error);
-            }
-        });
+        imageConfig.users[userId] = userImages
+        imageConfig.global = [...globalImages]
 
-        await message.reply(`Successfully added \"${imageLink}\" to ${category}[\"${memberId}\"]`)
+        await Promise.all([
+            imageConfig.save(),
+            message.reply(`Successfully added "${imageLink}" to ${message.author} images`)
+        ])
     },
 }
