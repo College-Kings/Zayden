1683d0233b3c2bf4c8dfe3d040e52e8f893bf99d
Oscar Six <26691547+KiloOscarSix@users.noreply.github.com>
2022-04-28 11:23:24 +0100

    created DB user config

---
+++ models/user-config.ts
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+
+export interface IUserConfig {
+    id: string
+    stars: {
+        numberOfStars: number,
+        givenStars: number,
+        receivedStars: number,
+    },
+    tradingCards: {
+        unopenedCardPacks: number,
+        openedCardPacks: number
+    }
+
+    save(): Promise<IUserConfig>;
+}
+
+export async function getUserConfig(id: string) {
+    return await UserConfig.findOne({id: id}).exec() || new UserConfig({id: id})
+}
+
+const UserConfigSchema = new mongoose.Schema({
+    id: String,
+    stars: {
+        numberOfStars: {type: Number, default: 0},
+        givenStars: {type: Number, default: 0},
+        receivedStars: {type: Number, default: 0},
+    },
+    tradingCards: {
+        unopenedCardPacks: {type: Number, default: 0},
+        openedCardPacks: {type: Number, default: 0}
+    }
+})
+
+export const UserConfig = mongoose.model("UserConfig", UserConfigSchema)
