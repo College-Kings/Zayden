9a3918d9ff49d5d243bb2658ccfe231fd1016994
Oscar Six <26691547+KiloOscarSix@users.noreply.github.com>
2022-04-28 12:07:54 +0100

    sort entire channel

--- commands/serious_interaction/fetchsuggestions.ts
+++ commands/serious_interaction/fetchsuggestions.ts
@@ -17,8 +17,22 @@ module.exports = {
             return message.reply("Invalid suggestion channel");
         }
 
-        const suggestionMessages = (await suggestionChannel.messages.fetch({limit: 100}))
-            .filter(message => message.author.id === '787490197943091211' && message.embeds.length > 0)
+        let suggestionMessages: Discord.Collection<string, Discord.Message> = new Discord.Collection();
+        let lastMessage;
+        let previousCollectionSize = -1;
+        let currentCollectionSize = suggestionMessages.size
+        while (previousCollectionSize != currentCollectionSize) {
+            previousCollectionSize = currentCollectionSize
+            suggestionMessages = suggestionMessages.concat((await suggestionChannel.messages.fetch({
+                limit: 100,
+                before: lastMessage?.id
+            })))
+            lastMessage = suggestionMessages.last()
+            currentCollectionSize = suggestionMessages.size
+            console.log(suggestionMessages.size)
+        }
+        suggestionMessages = suggestionMessages
+            .filter(message => message.author.id == message.client.user?.id && message.embeds.length > 0)
             .sort((a, b) => {
                 const a_thumbsUp = a.reactions.resolve('👍')
                 const b_thumbsUp = b.reactions.resolve('👍')
@@ -38,48 +52,39 @@ module.exports = {
                 }
             })
 
-        let index = 0
-        let count = 0;
+        let count = 1;
         let embed = new Discord.MessageEmbed()
             .setColor("#ff0000")
-            .setTitle(`Top ${suggestionMessages.size} suggestions!`)
-            .setDescription(`Here are the top ${suggestionMessages.size} suggestions, ordered by UP-Votes, excluding the Bot Vote!`);
+            .setTitle(`Top 10 suggestions!`)
+            .setDescription(`Here are the top 10 suggestions, ordered by Votes`);
 
         for (const [, element] of suggestionMessages) {
             const thumbsUp = element.reactions.resolve('👍')
             const thumbsDown = element.reactions.resolve('👎')
-            index++
 
             if (!thumbsUp || !thumbsDown) {
                 continue
             }
 
             embed.addField(
-                `Position: ${index}, 👍: ${thumbsUp.count - 1}, 👎: ${thumbsDown.count - 1}`,
+                `Position: ${count}, 👍: ${thumbsUp.count - 1}, 👎: ${thumbsDown.count - 1}`,
                 `Link: https://discord.com/channels/${guild.id}/${server.channels.suggestionChannel}/${element.id}`,
                 false
             );
 
-            count++
-
-            if (index == suggestionMessages.size) {
+            if (count == 10) {
                 embed.setImage("https://media.discordapp.net/attachments/769943204673486858/787791290514538516/CollegeKingsTopBanner.jpg?width=1440&height=360");
                 embed.setTimestamp();
 
                 await message.author.send({embeds: [embed]});
-            } else if (count > 20) {
-                await message.author.send({embeds: [embed]});
-
-                count = 0
-                embed = new Discord.MessageEmbed()
-                    .setColor("#ff0000")
-                    .setThumbnail("");
+                break;
             }
+            count++
         }
 
         const endTime = new Date();
 
-        statusMessage.edit(`Information sent in DMs, elapsed time: ${Math.round((endTime.getTime() - startTime.getTime()) / 1000)} second(s)!`);
+        statusMessage.edit(`Sent the top 10 suggestions out of ${suggestionMessages.size}, elapsed time: ${Math.round((endTime.getTime() - startTime.getTime()) / 1000)} second(s)!`);
     },
     permissions: ["MANAGE_MESSAGES"],
 }
