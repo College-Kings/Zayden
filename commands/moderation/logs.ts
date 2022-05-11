0441a7d960f024e0d33d1cbdc6243539c1944b76
Oscar Six <26691547+KiloOscarSix@users.noreply.github.com>
2022-05-11 22:35:11 +0100

    Add pages to logs

--- commands/moderation/logs.ts
+++ commands/moderation/logs.ts
@@ -1,6 +1,10 @@
-import {IServer} from "../../models/server";
-import Discord from "discord.js";
+import {IModeration, IServer} from "../../models/server";
+import Discord, {MessageActionRow, MessageButton} from "discord.js";
 import {setup} from "./functions";
+
+function getPageLogs(allLogs: IModeration[], pageNumber: number) {
+    return allLogs.slice((pageNumber - 1) * 5, pageNumber * 5)
+}
 
 module.exports = {
     commands: ["logs", "log"],
@@ -14,10 +18,12 @@ module.exports = {
             return
         }
 
-        let logMsg = ""
         const logs = server.moderation.filter(log => log.userId == member.id)
+        const numberOfPages = Math.ceil(logs.length / 5)
+        let pageNumber = 1
 
-        for (const log of logs) {
+        let logMsg = ""
+        for (const log of getPageLogs(logs, pageNumber)) {
             logMsg += `**Case ${log.caseNumber}**\n**Type:** ${log.logType}\n**User:** <@${log.userId}>\n**Moderator:** <@${log.moderatorId}>\n**Reason:** ${log.reason}\n\n`
         }
 
@@ -26,7 +32,70 @@ module.exports = {
             .setDescription(logMsg)
             .setColor("#ff0000")
 
-        message.channel.send({embeds: [embed]})
+        const nextPageButton = new MessageButton()
+            .setCustomId("nextPage")
+            .setLabel("Next Page")
+            .setStyle("PRIMARY")
+            .setDisabled(true);
+
+        if (numberOfPages > 1) {
+            nextPageButton.setDisabled(false)
+        }
+
+        const previousPageButton = new MessageButton()
+            .setCustomId("previousPage")
+            .setLabel("Previous Page")
+            .setStyle("PRIMARY")
+            .setDisabled(true);
+
+        const row = new MessageActionRow()
+            .addComponents(nextPageButton, previousPageButton);
+
+        const msg = await message.channel.send({embeds: [embed], components: [row]})
+
+        const filter = (interaction: Discord.MessageComponentInteraction) => (
+            interaction.customId == nextPageButton.customId
+            || interaction.customId == previousPageButton.customId
+            && interaction.user.id == message.author.id
+        );
+
+        const collector = msg.createMessageComponentCollector({filter});
+
+        collector.on("collect", i => {
+            console.log(`Interaction "${i.customId}" was clicked`)
+
+            // Next Page Interaction
+            if (i.customId == nextPageButton.customId) {
+                pageNumber += 1
+            }
+
+            // Previous Page Interaction
+            if (i.customId == previousPageButton.customId) {
+                pageNumber += 1
+            }
+
+            if (pageNumber + 1 > numberOfPages) {
+                nextPageButton.setDisabled(true)
+            } else {
+                nextPageButton.setDisabled(false)
+            }
+
+            if (pageNumber == 1) {
+                previousPageButton.setDisabled(true)
+            } else {
+                previousPageButton.setDisabled(false)
+            }
+
+            row.setComponents(nextPageButton, previousPageButton)
+
+            logMsg = ""
+            for (const log of getPageLogs(logs, pageNumber)) {
+                logMsg += `**Case ${log.caseNumber}**\n**Type:** ${log.logType}\n**User:** <@${log.userId}>\n**Moderator:** <@${log.moderatorId}>\n**Reason:** ${log.reason}\n\n`
+            }
+
+            embed.setDescription(logMsg)
+            i.update({embeds: [embed], components: [row]})
+        })
     },
     permissions: ["MANAGE_MESSAGES"],
 }
