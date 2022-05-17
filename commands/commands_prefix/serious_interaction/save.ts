082664664e69fe2708928d780998ba97dcde195c
Oscar Six <26691547+KiloOscarSix@users.noreply.github.com>
2022-05-17 17:37:04 +0100

    Updated channel ids

--- commands/commands_prefix/serious_interaction/save.ts
+++ commands/commands_prefix/serious_interaction/save.ts
@@ -1,8 +1,9 @@
 import Discord from "discord.js"
+import {IServer} from "../../../models/server";
 
 module.exports = {
     commands: ["save", "saves"],
-    callback: (message: Discord.Message) => {
-        message.channel.send("We do our best to retain save integrity with every update however due to the dynamic nature of game development saves might break. If you experience a save problem please let us know in <#747428614500384788>\n\nReminder:\nWith the major changes in v0.6 saves before this version will not work. We are sorry for the inconvenience. Use CTRL/TAB to quickly skip through the content you have seen")
+    callback: (message: Discord.Message, server: IServer) => {
+        message.channel.send(`We do our best to retain save integrity with every update however due to the dynamic nature of game development saves might break. If you experience a save problem please let us know in <#${server.channels.supportChannel}>\n\nReminder:\nWith the major changes in v0.6 saves before this version will not work. We are sorry for the inconvenience. Use CTRL/TAB to quickly skip through the content you have seen`)
     },
 }
--- commands/commands_prefix/serious_interaction/spoiler.ts
+++ commands/commands_prefix/serious_interaction/spoiler.ts
@@ -1,8 +1,9 @@
 import Discord from "discord.js"
+import {IServer} from "../../../models/server";
 
 module.exports = {
     commands: ["spoiler", "spoilers"],
-    callback: (message: Discord.Message) => {
-        message.channel.send("Please keep all conversations about the new update to <#770621445637799946>\nIf you have any bugs or questions please post them in <#747428614500384788> and <#888586464814854194>")
+    callback: (message: Discord.Message, server: IServer) => {
+        message.channel.send(`Please keep all conversations about the new update to <#770621445637799946>\nIf you have any bugs or questions please post them in <#${server.channels.supportChannel}>`)
     },
 }
