74d5631a618360fa365f679637635c3cf1b3bf26
Oscar Six <26691547+KiloOscarSix@users.noreply.github.com>
2022-11-29 03:28:19 +0000

    made callback async

--- commands/commands_message/fun_interactions/questionMe.ts
+++ commands/commands_message/fun_interactions/questionMe.ts
@@ -2,7 +2,7 @@ import Discord from "discord.js"
 
 module.exports = {
     command: "questionMe",
-    callback: (message: Discord.Message) => {
+    callback: async (message: Discord.Message) => {
         if (message.mentions.users.first()?.id == message.client.user?.id && message.content.slice(-1) == "?") {
             if (Math.floor(Math.random() * 2)) {
                 message.reply("Yes").then();
