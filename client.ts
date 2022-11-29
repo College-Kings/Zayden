067aadaf1706f88efa41408e026aa1baf7947e6b
Oscar Six <26691547+KiloOscarSix@users.noreply.github.com>
2022-11-29 03:27:10 +0000

    extend Discord.Client to handle custom properties

--- client.ts
+++ client.ts
@@ -1,7 +1,9 @@
 import Discord from "discord.js";
 import {ISlashCommand} from "./commands/commands_slash/command";
+import {IMessageCommand} from "./commands/commands_message/command";
 
 export class Zayden extends Discord.Client {
     slashCommands: Discord.Collection<string, ISlashCommand> = new Discord.Collection
+    messageCommands: Discord.Collection<string, IMessageCommand> = new Discord.Collection
 
 }
