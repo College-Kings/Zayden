ba48bcf8c4a1b459c59e1c09e76072fc2eeb1962
Oscar Six <26691547+KiloOscarSix@users.noreply.github.com>
2022-11-29 03:23:44 +0000

    update load_commands to support message commands and handle ignore files in compiled js

--- commands/load_commands.ts
+++ commands/load_commands.ts
@@ -3,14 +3,15 @@ import path from "path";
 import fs from "fs";
 import {Zayden} from "../client";
 import {ISlashCommand} from "./commands_slash/command";
+import {IMessageCommand} from "./commands_message/command";
 
-const ignoreFiles = ["command.ts", "command_base.ts", "functions.ts", "functions.ts"]
+const ignoreFiles = ["command", "command_base", "functions"]
 
 export function loadSlashCommands(client: Zayden) {
     client.slashCommands = new Discord.Collection();
 
     function readCommands(dir: string) {
-        const files = fs.readdirSync(path.join(__dirname, dir)).filter(file => !ignoreFiles.includes(file));
+        const files = fs.readdirSync(path.join(__dirname, dir)).filter(file => !ignoreFiles.includes(path.parse(file).name));
 
         for (const file of files) {
             const stat = fs.lstatSync(path.join(__dirname, dir, file))
@@ -21,8 +22,8 @@ export function loadSlashCommands(client: Zayden) {
 
             const command: ISlashCommand = require(path.join(__dirname, dir, file))
 
-            if ('data' in command && 'execute' in command) {
-                client.slashCommands!.set(command.data.name, command);
+            if ("data" in command && "execute" in command) {
+                client.slashCommands.set(command.data.name, command);
             } else {
                 console.log(`[WARNING] The command at ${file} is missing a required "data" or "execute" property.`);
             }
@@ -37,8 +38,10 @@ export function loadSlashCommands(client: Zayden) {
 }
 
 export function loadMessageCommands(client: Zayden) {
+    client.messageCommands = new Discord.Collection();
+
     function readCommands(dir: string) {
-        const files = fs.readdirSync(path.join(__dirname, dir)).filter(file => !ignoreFiles.includes(file));
+        const files = fs.readdirSync(path.join(__dirname, dir)).filter(file => !ignoreFiles.includes(path.parse(file).name));
 
         for (const file of files) {
             const stat = fs.lstatSync(path.join(__dirname, dir, file))
@@ -47,14 +50,19 @@ export function loadMessageCommands(client: Zayden) {
                 continue;
             }
 
-            const command = require(path.join(__dirname, dir, file))
+            const command: IMessageCommand = require(path.join(__dirname, dir, file))
 
-            if (client) {
-                const commandBase = require("./commands_message/command_base")
-                commandBase(client, command)
+            if ("command" in command && "callback" in command) {
+                client.messageCommands.set(command.command, command);
+            } else {
+                console.log(`[WARNING] The command at ${file} is missing a required "command" or "callback" property.`);
             }
         }
     }
 
     readCommands("./commands_message")
+
+    if (client) {
+        console.log(`Loaded ${client.messageCommands.size} message commands`)
+    }
 }
