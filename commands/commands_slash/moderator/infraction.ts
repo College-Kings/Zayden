87a29aeaee624dffa6f38daa4bdb370b00718aa8
Oscar Six <26691547+KiloOscarSix@users.noreply.github.com>
2023-01-22 16:12:40 +0000

    Moved to an infraction based system

---
+++ commands/commands_slash/moderator/infraction.ts
@@ -0,0 +1,71 @@
+import Discord from "discord.js"
+import {addLog, ban, LogType, mute, warn} from "./functions";
+import {getServer} from "../../../models/server";
+import {getUserConfig} from "../../../models/user-config";
+
+module.exports = {
+    data: new Discord.SlashCommandBuilder()
+        .setName("infraction")
+        .setDescription("Adds an infraction a user")
+        .setDefaultMemberPermissions(Discord.PermissionFlagsBits.ModerateMembers)
+        .addUserOption(option =>
+            option.setName("member")
+                .setDescription("Member to add infraction to")
+                .setRequired(true))
+        .addIntegerOption(option =>
+            option.setName("points")
+                .setDescription("Number of infraction points to give a user")
+                .setMinValue(0)
+                .setMaxValue(5))
+        .addStringOption(option =>
+            option.setName("reason")
+                .setDescription("Reason for the ban")),
+
+    async execute(interaction: Discord.ChatInputCommandInteraction) {
+        if (!interaction.guild) {
+            return;
+        }
+
+        const server = await getServer(interaction.guild.id)
+        const member = interaction.options.getMember("member")
+        const reason = interaction.options.getString("reason") ?? "No reason given"
+        const infractionPoints = interaction.options.getInteger("points") ?? 1
+
+        if (!(member instanceof Discord.GuildMember)) {
+            return interaction.reply({content: "Invalid member mention", ephemeral: true})
+        }
+
+        const user = await getUserConfig(member.id)
+
+        user.infractions = Math.min(infractionPoints + user.infractions, 5)
+        switch (user.infractions) {
+            case 1:
+                await warn(server, interaction.guild, interaction.channel!, member, interaction.user, reason);
+                break;
+            case 2:
+                await mute(server, interaction.guild, interaction.channel!, member, interaction.user, 3600000, reason)
+                break;
+            case 3:
+                await mute(server, interaction.guild, interaction.channel!, member, interaction.user, 28800000, reason)
+                break;
+            case 4:
+                await mute(server, interaction.guild, interaction.channel!, member, interaction.user, 604800000, reason)
+                break;
+            case 5:
+                await ban(server, interaction.guild, interaction.channel!, member, interaction.user, reason)
+                break;
+            default:
+                break;
+        }
+
+        const response = new Discord.EmbedBuilder()
+            .setTitle("Added Infraction")
+            .setDescription(`${member} has been given ${infractionPoints} infraction points`)
+            .setColor("#ff0000")
+
+        await Promise.all([
+            addLog(server, LogType.Infraction, interaction.guild, member, interaction.user, reason),
+            interaction.reply({embeds: [response], ephemeral: true})
+        ])
+    }
+}
