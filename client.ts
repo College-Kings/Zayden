418e9253b17ed2f9fa564fe21689ba84d505ee53
Oscar Six <26691547+KiloOscarSix@users.noreply.github.com>
2023-01-29 00:37:48 +0000

    updated client

--- client.ts
+++ client.ts
@@ -3,6 +3,11 @@ import {ISlashCommand} from "./commands/commands_slash/command";
 import {IMessageCommand} from "./commands/commands_message/command";
 
 export class Zayden extends Discord.Client {
+    version = [1, 0, 0]
+    developers = ["211486447369322506"]
+    activities: Discord.ActivityOptions[] = [
+        {name: "College Kings", type: Discord.ActivityType.Playing, url: "https://www.patreon.com/collegekings"}
+    ]
     slashCommands: Discord.Collection<string, ISlashCommand> = new Discord.Collection
     messageCommands: Discord.Collection<string, IMessageCommand> = new Discord.Collection
 
