202d2df725937e78c956c6047a38bc72bdf7578f
Oscar Six <26691547+KiloOscarSix@users.noreply.github.com>
2023-02-01 07:31:00 +0000

    added enabledModules array

--- models/server_settings/MiscellaneousSchema.ts
+++ models/server_settings/MiscellaneousSchema.ts
@@ -1,12 +1,14 @@
 import mongoose from "mongoose";
 
 export interface IMiscellaneous {
-    supportThreadId: number;
-    moderationRoles: string[]
+    enabledModules: string[]
+    moderationRoles: string[];
     supportRoles: string[]
+    supportThreadId: number
 }
 
 export const MiscellaneousSchema = new mongoose.Schema<IMiscellaneous>({
+    enabledModules: [String],
     supportThreadId: {type: Number, default: 0},
     moderationRoles: [String],
     supportRoles: [String]
