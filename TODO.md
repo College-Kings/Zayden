4b3039353a4c42226b3e5aa62ce1ee8e98fdd92b
Oscar Six <26691547+KiloOscarSix@users.noreply.github.com>
2023-02-01 07:44:57 +0000

    updated version and todo list

--- TODO.md
+++ TODO.md
@@ -2,12 +2,9 @@
 
 ## Zayden v4
 
-- Fix !WisdomOfTheDay command
 - Fix event logs
-- Improve moderation commands
 
 ## Zayden v5
 
-- Move to slash/interaction api
 - Replace mee6
 - Replace dyno
--- client.ts
+++ client.ts
@@ -3,7 +3,7 @@ import {ISlashCommand} from "./commands/commands_slash/command";
 import {IMessageCommand} from "./commands/commands_message/command";
 
 export class Zayden extends Discord.Client {
-    version = [1, 0, 0]
+    version = [4, 2, 0]
     developers = ["211486447369322506"]
     activities: Discord.ActivityOptions[] = [
         {name: "College Kings", type: Discord.ActivityType.Playing, url: "https://www.patreon.com/collegekings"}
