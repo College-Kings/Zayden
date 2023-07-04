4afcca013312ad174e727af43c4bceb0863c9a06
Oscar Six <26691547+KiloOscarSix@users.noreply.github.com>
2023-07-04 15:47:13 +0100

    update formatting

--- src/commands/message_commands/ai_chat.rs
+++ src/commands/message_commands/ai_chat.rs
@@ -1,16 +1,19 @@
+use crate::chatgpt_lib;
 use serenity::model::channel::Message;
 use serenity::prelude::*;
-use crate::chatgpt_lib;
 
 pub async fn run(ctx: &Context, msg: &Message) {
     if msg.content.ends_with("?") {
         if let Some(mention) = msg.mentions.first() {
             if mention.id == ctx.cache.current_user_id() {
+                let response: chatgpt_lib::ChatResponse =
+                    chatgpt_lib::chat(&msg.content, &msg.author.name)
+                        .await
+                        .unwrap();
 
-                let response: chatgpt_lib::ChatResponse = chatgpt_lib::chat(&msg.content, &msg.author.name).await.unwrap();
-
-                msg.reply(&ctx, &response.choices[0].message.content).await.unwrap();
-
+                msg.reply(&ctx, &response.choices[0].message.content)
+                    .await
+                    .unwrap();
             }
         }
     }
