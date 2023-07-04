3dbc0bb51686160a7f0b006bdb59b0923141a1c3
Oscar Six <26691547+KiloOscarSix@users.noreply.github.com>
2023-07-04 19:00:55 +0100

    created api functions for support_thread_id

--- src/sqlx_lib.rs
+++ src/sqlx_lib.rs
@@ -1,6 +1,6 @@
 use crate::models::Image;
 use sqlx::postgres::PgPoolOptions;
-use sqlx::PgPool;
+use sqlx::{Error, PgPool};
 use std::env;
 
 async fn get_pool() -> PgPool {
@@ -26,4 +26,34 @@ pub async fn get_good_morning_images() -> Vec<Image> {
 
 pub async fn get_good_night_images() -> Vec<Image> {
     fetch_images("SELECT * FROM good_night_images").await
+}
+
+pub async fn get_support_thead_id(server_id: i64) -> Result<i32, Error> {
+    let pool = get_pool().await;
+
+    let result = sqlx::query!("SELECT support_thread_id FROM servers WHERE id = $1", server_id)
+        .fetch_one(&pool)
+        .await?;
+
+    Ok(result.support_thread_id)
+}
+
+pub async fn post_support_thread_id(server_id: i64, thread_id: i32) -> Result<(), Error> {
+    let pool = get_pool().await;
+
+    sqlx::query!("INSERT INTO servers (id, support_thread_id) VALUES ($1, $2)", server_id, thread_id)
+        .execute(&pool)
+        .await?;
+
+    Ok(())
+}
+
+pub async fn update_support_thread_id(server_id: i64, thread_id: i32) -> Result<(), Error> {
+    let pool = get_pool().await;
+
+    sqlx::query!("UPDATE servers SET support_thread_id = $1 WHERE id = $2", thread_id, server_id)
+        .execute(&pool)
+        .await?;
+
+    Ok(())
 }
