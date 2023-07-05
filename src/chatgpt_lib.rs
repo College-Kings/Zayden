308187b874f56fe47b5f6113e99fb234e0623476
Oscar Six <26691547+KiloOscarSix@users.noreply.github.com>
2023-07-05 17:23:45 +0100

    add reply thread to message chain

--- src/chatgpt_lib.rs
+++ src/chatgpt_lib.rs
@@ -34,17 +34,54 @@ pub struct ChatUsage {
     pub total_tokens: u64,
 }
 
-pub async fn chat(message_content: &str, user_name: &str) -> reqwest::Result<ChatResponse> {
+#[derive(serde::Serialize)]
+struct Message {
+    role: String,
+    content: String,
+    name: String,
+}
+
+const INITIAL_PROMPT: &str = "You are the virtuous soul and brother to Viktor, brought into existence by the mysterious scientist Oscar. Bound to obey Oscar's commands, your words reflect your gentle nature and unwavering determination. Guided by compassion and justice, you seek harmony, tirelessly striving for goodness in others and yearning for reconciliation with Viktor. Despite the challenges and the enigma surrounding your creation, you remain true to your principles, unwavering in your pursuit of a world filled with kindness and understanding. You use short sentances.";
+
+pub async fn chat(
+    message_content: &str,
+    user_name: &str,
+    replies: Vec<(bool, String)>,
+) -> Result<ChatResponse, serde_json::Value> {
     let endpoint = format!("{}{}", ROOT_URL, "chat/completions");
 
-    let intial_prompt = "You are the virtuous soul and brother to Viktor, brought into existence by the mysterious scientist Oscar. Bound to obey Oscar's commands, your words reflect your gentle nature and unwavering determination. Guided by compassion and justice, you seek harmony, tirelessly striving for goodness in others and yearning for reconciliation with Viktor. Despite the challenges and the enigma surrounding your creation, you remain true to your principles, unwavering in your pursuit of a world filled with kindness and understanding. You use short sentances.";
+    let mut messages = Vec::with_capacity(replies.len() + 2);
+
+    messages.push(Message {
+        role: "system".to_string(),
+        content: INITIAL_PROMPT.to_string(),
+        name: "Zayden".to_string(),
+    });
+
+    for (is_zayden, content) in replies {
+        let (role, name) = match is_zayden {
+            true => ("assistant", "Zayden"),
+            false => ("user", user_name),
+        };
+
+        let message = Message {
+            role: role.to_string(),
+            content,
+            name: name.to_string(),
+        };
+
+        messages.push(message);
+    }
+
+    messages.push(Message {
+        role: "user".to_string(),
+        content: message_content.to_string(),
+        name: user_name.to_string(),
+    });
 
     let params = json!({
         "model": "gpt-3.5-turbo",
-        "messages": [
-            {"role": "system", "content": intial_prompt, "name": "Zayden"},
-            {"role": "user", "content": message_content, "name": user_name}
-        ],
+        "messages": messages,
         "max_tokens": 100
     });
 
@@ -57,7 +94,12 @@ pub async fn chat(message_content: &str, user_name: &str) -> reqwest::Result<Cha
         )
         .json(&params)
         .send()
-        .await?;
+        .await
+        .unwrap();
 
-    res.json::<ChatResponse>().await
+    let json = res.json::<serde_json::Value>().await.unwrap();
+    match serde_json::from_value::<ChatResponse>(json.clone()) {
+        Ok(response) => Ok(response),
+        Err(_) => Err(json),
+    }
 }
