6e6c667f597397abea7b4bbea2872371e4eb86b5
Oscar Six <26691547+KiloOscarSix@users.noreply.github.com>
2023-07-10 02:01:17 +0100

    created fetch_suggestions command

--- src/commands/slash_commands/fetch_suggestions.rs
+++ src/commands/slash_commands/fetch_suggestions.rs
@@ -15,12 +15,34 @@ pub async fn run(ctx: &Context, interaction: &ApplicationCommandInteraction) -> 
 
     interaction.defer(&ctx).await.unwrap();
 
+    // TODO: Included archived threads. This is currently not possible with the current version of the library.
+
     let active_threads = guild_id.get_active_threads(&ctx).await.unwrap();
     let suggestion_threads = active_threads.threads.iter().filter(|thread| thread.parent_id == Some(ChannelId(SUGGESTION_CHANNEL_ID))).collect::<Vec<_>>();
+    let mut thread_reaction_counts = Vec::with_capacity(suggestion_threads.len());
 
     for thread in suggestion_threads {
-        let reactions = thread.reaction_users(&ctx, thread.id.0, ReactionType::Unicode("👍".to_string()), None, None).await.unwrap();
-        println!("{:?}", reactions);
+        let count = thread.reaction_users(&ctx, thread.id.0, ReactionType::Unicode("👍".to_string()), Some(100), None).await.unwrap().iter().len();
+        thread_reaction_counts.push((thread, count));
+    }
+
+    thread_reaction_counts.sort_by(|a, b| b.1.cmp(&a.1));
+
+
+    let result = interaction.user.dm(&ctx, |f| {
+        f.embed(|e| {
+            e.title("Top 10 suggestions");
+            e.description("Here are the top 10 suggestions, sorted by upvotes.");
+
+            for (i, (thread, count)) in thread_reaction_counts[..10].iter().enumerate() {
+                e.field(format!("{}. 👍: {}", i + 1, count), format!("Link: {}", thread), false);
+            }
+            e
+        })
+    }).await;
+
+    if result.is_err() {
+        return respond_with_message(ctx, interaction, "I couldn't DM you. Please enable DMs from server members and try again.").await;
     }
 
     edit_response_with_message(ctx, interaction, "Suggestions fetched").await.expect("Error editing response");
