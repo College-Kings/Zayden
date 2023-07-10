45cf5e4d5452293ad3a0fa2f5a6031bd7b408795
Oscar Six <26691547+KiloOscarSix@users.noreply.github.com>
2023-07-10 03:12:16 +0100

    Fixed and optimised fetch_suggestions

--- src/commands/slash_commands/fetch_suggestions.rs
+++ src/commands/slash_commands/fetch_suggestions.rs
@@ -1,33 +1,52 @@
+use std::string::ToString;
+use std::time;
 use serenity::builder::CreateApplicationCommand;
 use serenity::model::id::ChannelId;
 use serenity::model::prelude::application_command::ApplicationCommandInteraction;
-use serenity::model::prelude::ReactionType;
+use serenity::model::prelude::{GuildChannel, ReactionType};
 use serenity::prelude::Context;
 use crate::utils::{edit_response_with_message, respond_with_message};
 
 const SUGGESTION_CHANNEL_ID: u64 = 1068790374996377671;
 
 pub async fn run(ctx: &Context, interaction: &ApplicationCommandInteraction) -> Result<(), serenity::Error> {
+    let start_time = time::Instant::now();
+
     let guild_id = match interaction.guild_id {
         Some(guild_id) => guild_id,
         None => return respond_with_message(ctx, interaction, "This command can only be used in a server").await,
     };
 
-    interaction.defer(&ctx).await.unwrap();
+    let suggestion_channel = ChannelId(SUGGESTION_CHANNEL_ID);
 
-    // TODO: Included archived threads. This is currently not possible with the current version of the library.
+    interaction.defer(&ctx).await?;
 
-    let active_threads = guild_id.get_active_threads(&ctx).await.unwrap();
-    let suggestion_threads = active_threads.threads.iter().filter(|thread| thread.parent_id == Some(ChannelId(SUGGESTION_CHANNEL_ID))).collect::<Vec<_>>();
-    let mut thread_reaction_counts = Vec::with_capacity(suggestion_threads.len());
+    let active_guild_threads = guild_id.get_active_threads(&ctx).await?;
+    let mut threads: Vec<GuildChannel> = Vec::new();
 
-    for thread in suggestion_threads {
-        let count = thread.reaction_users(&ctx, thread.id.0, ReactionType::Unicode("👍".to_string()), Some(100), None).await.unwrap().iter().len();
-        thread_reaction_counts.push((thread, count));
+    for thread in active_guild_threads.threads {
+        if thread.parent_id == Some(suggestion_channel) {
+            threads.push(thread);
+        }
+    }
+
+    for thread in suggestion_channel.get_archived_public_threads(&ctx, None, None).await?.threads {
+        threads.push(thread);
+    }
+
+    let mut thread_reaction_counts = Vec::with_capacity(threads.len());
+
+    for thread in threads {
+        let reactions = match thread.reaction_users(&ctx, thread.id.0, ReactionType::Unicode("👍".to_string()), Some(100), None).await {
+            Ok(reactions) => reactions,
+            Err(_) => continue,
+        };
+        thread_reaction_counts.push((thread, reactions.len()));
     }
 
     thread_reaction_counts.sort_by(|a, b| b.1.cmp(&a.1));
 
+    let elapsed_time = start_time.elapsed();
 
     let result = interaction.user.dm(&ctx, |f| {
         f.embed(|e| {
@@ -45,7 +64,7 @@ pub async fn run(ctx: &Context, interaction: &ApplicationCommandInteraction) -> 
         return respond_with_message(ctx, interaction, "I couldn't DM you. Please enable DMs from server members and try again.").await;
     }
 
-    edit_response_with_message(ctx, interaction, "Suggestions fetched").await.expect("Error editing response");
+    edit_response_with_message(ctx, interaction, &format!("Suggestions fetched. Took {} seconds", elapsed_time.as_secs())).await.expect("Error editing response");
     Ok(())
 }
 
