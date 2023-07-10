18b784c1d2188f1e1eacbdf324eef7b49293eb0c
Oscar Six <26691547+KiloOscarSix@users.noreply.github.com>
2023-07-10 20:05:31 +0100

    removed unnecessary search calls

--- src/commands/slash_commands/answer.rs
+++ src/commands/slash_commands/answer.rs
@@ -1,6 +1,7 @@
 use serenity::builder::CreateApplicationCommand;
 use serenity::model::Permissions;
 use serenity::model::prelude::application_command::{ApplicationCommandInteraction, CommandDataOptionValue};
+use serenity::model::prelude::ChannelId;
 use serenity::model::prelude::command::CommandOptionType;
 use serenity::prelude::Context;
 use crate::sqlx_lib::update_question_answer;
@@ -9,11 +10,6 @@ use crate::utils::respond_with_message;
 const QUESTION_CHANNEL_ID: u64 = 829463308629180447;
 
 pub async fn run(ctx: &Context, interaction: &ApplicationCommandInteraction) -> Result<(), serenity::Error> {
-    let guild_id = match interaction.guild_id {
-        Some(guild_id) => guild_id,
-        None => return respond_with_message(ctx, interaction, "This command can only be used in a server").await,
-    };
-
     let id = match interaction.data.options[0].resolved.as_ref() {
         Some(CommandDataOptionValue::Integer(id)) => id,
         _ => return respond_with_message(ctx, interaction, "Invalid question ID").await,
@@ -29,8 +25,7 @@ pub async fn run(ctx: &Context, interaction: &ApplicationCommandInteraction) -> 
         Err(_) => return respond_with_message(ctx, interaction, "Error updating question").await,
     };
 
-    let guild_channels = guild_id.channels(&ctx).await.unwrap();
-    let question_channel = guild_channels.values().find(|channel| channel.id.0 == QUESTION_CHANNEL_ID).unwrap();
+    let question_channel = ChannelId(QUESTION_CHANNEL_ID);
 
     let mut msg = question_channel.message(ctx, question.message_id.unwrap() as u64).await.unwrap();
     let msg_result = msg.edit(ctx, |message| {
--- src/commands/slash_commands/question.rs
+++ src/commands/slash_commands/question.rs
@@ -1,5 +1,6 @@
 use serenity::builder::CreateApplicationCommand;
 use serenity::model::prelude::application_command::{ApplicationCommandInteraction, CommandDataOptionValue};
+use serenity::model::prelude::ChannelId;
 use serenity::model::prelude::command::CommandOptionType;
 use serenity::prelude::Context;
 use crate::sqlx_lib::{create_question, update_question_message_id};
@@ -8,21 +9,12 @@ use crate::utils::{respond_with_ephemeral_message, respond_with_message};
 const QUESTION_CHANNEL_ID: u64 = 829463308629180447;
 
 pub async fn run(ctx: &Context, interaction: &ApplicationCommandInteraction) -> Result<(), serenity::Error> {
-    let guild_id = match interaction.guild_id {
-        Some(guild_id) => guild_id,
-        None => return respond_with_message(ctx, interaction, "This command can only be used in a server").await,
-    };
-
     let question = match interaction.data.options[0].resolved.as_ref() {
         Some(CommandDataOptionValue::String(question)) => question,
         _ => return respond_with_message(ctx, interaction, "Invalid question").await,
     };
 
-    let guild_channels = guild_id.channels(&ctx).await.unwrap();
-    let question_channel = match guild_channels.values().find(|channel| channel.id.0 == QUESTION_CHANNEL_ID) {
-        Some(question_channel) => question_channel,
-        None => return respond_with_message(ctx, interaction, "Error finding question channel").await,
-    };
+    let question_channel = ChannelId(QUESTION_CHANNEL_ID);
 
     let question = match create_question(question, interaction.user.id.0 as i64).await {
         Ok(question) => question,
