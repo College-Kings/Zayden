8811653ec1b03bd4b58f183df2f5d18a914b7eda
Oscar Six <26691547+KiloOscarSix@users.noreply.github.com>
2023-11-22 02:57:08 +0000

    Refactor add_artist command

--- src/commands/slash_commands/add_artist.rs
+++ src/commands/slash_commands/add_artist.rs
@@ -1,16 +1,30 @@
-use serenity::builder::CreateApplicationCommand;
-use serenity::model::Permissions;
-use serenity::model::prelude::application_command::{ApplicationCommandInteraction, CommandDataOptionValue};
-use serenity::model::prelude::command::CommandOptionType;
-use serenity::prelude::Context;
 use crate::utils::respond_with_message;
+use serenity::builder::CreateApplicationCommand;
+use serenity::model::prelude::application_command::{
+    ApplicationCommandInteraction, CommandDataOptionValue,
+};
+use serenity::model::prelude::command::CommandOptionType;
+use serenity::model::Permissions;
+use serenity::prelude::Context;
 
 const ARTIST_ROLE_ID: u64 = 1043987303556726854;
 
-pub async fn run(ctx: &Context, interaction: &ApplicationCommandInteraction) -> Result<(), serenity::Error> {
+pub async fn run(
+    ctx: &Context,
+    interaction: &ApplicationCommandInteraction,
+) -> serenity::Result<()> {
+    // ) -> Result<(), serenity::Error> {
+
     let guild_id = match interaction.guild_id {
         Some(guild_id) => guild_id,
-        None => return respond_with_message(ctx, interaction, "This command can only be used in a server").await,
+        None => {
+            return respond_with_message(
+                ctx,
+                interaction,
+                "This command can only be used in a server",
+            )
+            .await
+        }
     };
 
     let user = match interaction.data.options[0].resolved.as_ref() {
@@ -24,17 +38,21 @@ pub async fn run(ctx: &Context, interaction: &ApplicationCommandInteraction) -> 
     };
 
     match member.add_role(ctx, ARTIST_ROLE_ID).await {
-        Ok(_) => respond_with_message(ctx, interaction, &format!("Added {} as an artist", user)).await,
+        Ok(_) => {
+            respond_with_message(ctx, interaction, &format!("Added {} as an artist", user)).await
+        }
         Err(_) => respond_with_message(ctx, interaction, "Error adding role to member").await,
     }
 }
 
 pub fn register(command: &mut CreateApplicationCommand) -> &mut CreateApplicationCommand {
-    command.name("add_artist")
+    command
+        .name("add_artist")
         .description("Adds a user as an artist")
         .default_member_permissions(Permissions::MANAGE_MESSAGES)
         .create_option(|option| {
-            option.name("user")
+            option
+                .name("user")
                 .description("The user to add as an artist")
                 .kind(CommandOptionType::User)
                 .required(true)
