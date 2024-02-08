78399fecb5136753525c89a349cb606adbd70127
Oscar Six <26691547+KiloOscarSix@users.noreply.github.com>
2024-02-08 02:38:47 +0000

    Update GM/GN to reply to command

--- src/commands/slash_commands/good_morning.rs
+++ src/commands/slash_commands/good_morning.rs
@@ -1,17 +1,18 @@
-use crate::utils::send_embed;
 use crate::ImageCache;
 use chrono::{Duration, Local};
 use rand::seq::SliceRandom;
 use rand::thread_rng;
 use serenity::all::{
-    CommandInteraction, Context, CreateAttachment, CreateCommand, CreateEmbed, CreateMessage,
-    Message,
+    CommandInteraction, Context, CreateAttachment, CreateCommand, CreateEmbed, EditAttachments,
+    EditInteractionResponse, Message,
 };
 
 pub async fn run(
     ctx: Context,
     interaction: &CommandInteraction,
 ) -> Result<Message, serenity::Error> {
+    interaction.defer(&ctx).await.expect("Failed to defer");
+
     let mut data = ctx.data.write().await;
     let image_cache = data.get_mut::<ImageCache>().unwrap();
 
@@ -25,18 +26,20 @@ pub async fn run(
     let image_path = entries.choose(&mut thread_rng()).unwrap();
     let file_name = image_path.file_name().unwrap().to_str().unwrap();
 
-    send_embed(
-        &ctx,
-        interaction,
-        CreateMessage::new()
-            .embed(
-                CreateEmbed::new()
-                    .title(format!("Good Morning, {}!", interaction.user.name))
-                    .attachment(file_name),
-            )
-            .add_file(CreateAttachment::path(image_path).await.unwrap()),
-    )
-    .await
+    interaction
+        .edit_response(
+            &ctx,
+            EditInteractionResponse::new()
+                .embed(
+                    CreateEmbed::new()
+                        .title(format!("Good Morning, {}!", interaction.user.name))
+                        .attachment(file_name),
+                )
+                .attachments(
+                    EditAttachments::new().add(CreateAttachment::path(image_path).await.unwrap()),
+                ),
+        )
+        .await
 }
 
 pub fn register() -> CreateCommand {
