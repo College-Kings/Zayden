a8989e6a1471693a8f2e84c5123f065fa08acb02
Oscar Six <26691547+KiloOscarSix@users.noreply.github.com>
2024-03-04 05:10:05 +0000

    Add image filtering base upon role name

--- src/commands/slash_commands/image.rs
+++ src/commands/slash_commands/image.rs
@@ -1,3 +1,5 @@
+use std::path::PathBuf;
+
 use crate::image_cache::ImageCache;
 use chrono::{Duration, Local};
 use rand::seq::SliceRandom;
@@ -20,24 +22,44 @@ pub async fn run(
         image_cache.update().await;
     };
 
-    let entries = [
-        image_cache.good_night_images.as_slice(),
-        image_cache.good_morning_images.as_slice(),
-    ]
-    .concat();
+    let guild_roles = interaction
+        .guild_id
+        .unwrap()
+        .to_partial_guild(&ctx)
+        .await?
+        .roles;
+    let member_roles = &interaction.member.as_ref().unwrap().roles;
 
-    let image_path = entries.choose(&mut thread_rng()).unwrap();
+    let entries: Vec<&PathBuf> = member_roles
+        .iter()
+        .map(|role_id| {
+            let role = guild_roles.get(role_id).unwrap();
+            &role.name
+        })
+        .filter_map(|name| {
+            if let Some(name) = name.split('\'').next() {
+                image_cache.character_map.get(&name.to_lowercase())
+            } else {
+                None
+            }
+        })
+        .flatten()
+        .collect();
+
+    let image_path = match entries.choose(&mut thread_rng()) {
+        Some(path) => path,
+        None => {
+            let images = &image_cache.good_morning_images;
+            images.choose(&mut thread_rng()).unwrap()
+        }
+    };
     let file_name = image_path.file_name().unwrap().to_str().unwrap();
 
     interaction
         .edit_response(
             &ctx,
             EditInteractionResponse::new()
-                .embed(
-                    CreateEmbed::new()
-                        .title(format!("Good Night, {}!", interaction.user.name))
-                        .attachment(file_name),
-                )
+                .embed(CreateEmbed::new().attachment(file_name))
                 .attachments(
                     EditAttachments::new().add(CreateAttachment::path(image_path).await.unwrap()),
                 ),
