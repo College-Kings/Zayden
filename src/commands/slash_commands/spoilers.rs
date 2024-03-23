ca48cafd0a51ee307979b17f7583070a5384ed54
Oscar Six <26691547+KiloOscarSix@users.noreply.github.com>
2024-03-23 04:02:34 +0000

    Refactor spoilers command to use message_response

--- src/commands/slash_commands/spoilers.rs
+++ src/commands/slash_commands/spoilers.rs
@@ -1,9 +1,11 @@
 use crate::sqlx_lib::{get_spoiler_channel_ids, get_support_channel_ids};
-use crate::utils::send_message;
+use crate::utils::message_response;
 use crate::{Error, Result};
 use serenity::all::{CommandInteraction, Context, CreateCommand};
 
 pub async fn run(ctx: &Context, interaction: &CommandInteraction) -> Result<()> {
+    interaction.defer(ctx).await?;
+
     let guild_id = interaction.guild_id.ok_or_else(|| Error::NoGuild)?;
 
     let support_thread_ids = get_support_channel_ids(guild_id.get() as i64).await?;
@@ -16,7 +18,9 @@ pub async fn run(ctx: &Context, interaction: &CommandInteraction) -> Result<()> 
         .first()
         .ok_or_else(|| Error::NoSpoilerThread)?;
 
-    send_message(ctx, interaction, &format!("Please keep all conversations about the new update to <#{}>\nIf you have any bugs or questions please post them in <#{}>", spoiler_thread_id, support_thread_id)).await?;
+    message_response(ctx, interaction, &format!("Spoilers are defined as any content that has not been released on all supported platforms for at least 2 weeks.
+    Please keep all conversations about spoilers to <#{}>
+    If you have any bugs or questions please post them in <#{}>", spoiler_thread_id, support_thread_id)).await?;
 
     Ok(())
 }
