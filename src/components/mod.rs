428bb4b3775c4fe328df0e6b7c16c29b2895ec3d
Oscar Six <26691547+KiloOscarSix@users.noreply.github.com>
2024-03-27 01:38:07 +0000

    Add support_ticket module

--- src/components/mod.rs
+++ src/components/mod.rs
@@ -1,39 +1,7 @@
 pub mod availability_check;
+mod production_request;
+mod support_ticket;
 
 pub use availability_check::availability_check;
-use serenity::all::{
-    ComponentInteraction, Context, CreateActionRow, CreateInputText, CreateInteractionResponse,
-    CreateModal, InputTextStyle,
-};
-
-use crate::Result;
-
-pub async fn support_ticket(ctx: &Context, interaction: &ComponentInteraction) -> Result<()> {
-    let version_input = CreateInputText::new(InputTextStyle::Short, "Game Version", "version")
-        .required(true)
-        .placeholder("1.0.0");
-
-    let issue_input = CreateInputText::new(InputTextStyle::Paragraph, "Issue", "issue")
-        .required(true)
-        .placeholder("Describe the issue you're experiencing");
-
-    let additional_input = CreateInputText::new(
-        InputTextStyle::Paragraph,
-        "Additional Information",
-        "additional",
-    )
-    .required(false)
-    .placeholder("Please send a save file that replicates the issue once the ticket is created.");
-
-    let modal = CreateModal::new("support_ticket", "Support Ticket").components(vec![
-        CreateActionRow::InputText(version_input),
-        CreateActionRow::InputText(issue_input),
-        CreateActionRow::InputText(additional_input),
-    ]);
-
-    interaction
-        .create_response(&ctx, CreateInteractionResponse::Modal(modal))
-        .await?;
-
-    Ok(())
-}
+pub use production_request::production_request;
+pub use support_ticket::support_ticket;
