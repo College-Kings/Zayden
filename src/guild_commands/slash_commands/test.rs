8c8735a5d5fc61b6a56a6ff802c56ce31c5bab90
Oscar Six <26691547+KiloOscarSix@users.noreply.github.com>
2024-03-27 01:38:18 +0000

    Add button to send production request

--- src/guild_commands/slash_commands/test.rs
+++ src/guild_commands/slash_commands/test.rs
@@ -1,9 +1,20 @@
-use serenity::all::{CommandInteraction, Context, CreateCommand, GuildId, Permissions};
+use serenity::all::{
+    ChannelId, CommandInteraction, Context, CreateButton, CreateCommand, CreateMessage, GuildId,
+    Permissions,
+};
 
 use crate::{utils::message_response, Result, COLLEGE_KINGS_GUILD_ID};
 
 pub async fn run(ctx: &Context, interaction: &CommandInteraction) -> Result<()> {
-    message_response(ctx, interaction, "Sent test command").await?;
+    ChannelId::new(1164928484838227968)
+        .send_message(
+            ctx,
+            CreateMessage::default()
+                .button(CreateButton::new("production_request").label("Production Request")),
+        )
+        .await?;
+
+    message_response(ctx, interaction, "Success").await?;
     Ok(())
 }
 
