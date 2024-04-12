ee9f4c2069f8e423281de3a53abb86c03b894f42
Oscar Six <26691547+KiloOscarSix@users.noreply.github.com>
2024-04-12 17:33:49 +0100

    Refactor levels module to use PostgresPool in user_levels.rs

--- src/sqlx_lib/user_levels.rs
+++ src/sqlx_lib/user_levels.rs
@@ -1,9 +1,7 @@
 use chrono::NaiveDateTime;
+use sqlx::{Pool, Postgres};
 
-use crate::sqlx_lib;
 use crate::{Error, Result};
-
-pub const LIMIT: i64 = 10;
 
 pub struct UserLevelData {
     pub id: i64,
@@ -14,13 +12,14 @@ pub struct UserLevelData {
     pub last_xp: NaiveDateTime,
 }
 
-pub async fn get_user_level_data<T: TryInto<i64>>(user_id: T) -> Result<UserLevelData> {
-    let pool = sqlx_lib::get_pool().await?;
-
+pub async fn get_user_level_data(
+    pool: &Pool<Postgres>,
+    user_id: impl TryInto<i64>,
+) -> Result<UserLevelData> {
     let user_id: i64 = user_id.try_into().map_err(|_| Error::ConversionError)?;
 
     let data = match sqlx::query_as!(UserLevelData, "SELECT * FROM levels WHERE id = $1", user_id)
-        .fetch_optional(&pool)
+        .fetch_optional(pool)
         .await?
     {
         Some(data) => data,
@@ -30,7 +29,7 @@ pub async fn get_user_level_data<T: TryInto<i64>>(user_id: T) -> Result<UserLeve
                 "INSERT INTO levels (id) VALUES ($1) RETURNING *",
                 user_id,
             )
-            .fetch_one(&pool)
+            .fetch_one(pool)
             .await?
         }
     };
@@ -39,68 +38,67 @@ pub async fn get_user_level_data<T: TryInto<i64>>(user_id: T) -> Result<UserLeve
 }
 
 pub async fn update_user_level_data(
-    user_id: i64,
+    pool: &Pool<Postgres>,
+    user_id: impl TryInto<i64>,
     xp: i32,
     total_xp: i32,
     level: i32,
 ) -> Result<()> {
-    let pool = sqlx_lib::get_pool().await?;
-
     sqlx::query!(
         "UPDATE levels SET xp = $1, total_xp = $2, level = $3, message_count = message_count + 1, last_xp = now() WHERE id = $4",
         xp,
         total_xp,
         level,
-        user_id
+        user_id.try_into().map_err(|_| Error::ConversionError)?
     )
-    .execute(&pool)
+    .execute(pool)
     .await?;
 
     Ok(())
 }
 
-pub async fn get_user_rank<T: TryInto<i64>>(user_id: T) -> Result<Option<i64>> {
-    let pool = sqlx_lib::get_pool().await?;
-
-    let user_id: i64 = user_id.try_into().map_err(|_| Error::ConversionError)?;
-
-    let rank = sqlx::query!(
+pub async fn get_user_rank(
+    pool: &Pool<Postgres>,
+    user_id: impl TryInto<i64>,
+) -> Result<Option<i64>> {
+    let data = sqlx::query!(
         "SELECT rank FROM (SELECT id, RANK() OVER (ORDER BY total_xp DESC) FROM levels) AS ranked WHERE id = $1",
-        user_id
+        user_id.try_into().map_err(|_| Error::ConversionError)?
     )
-    .fetch_one(&pool)
+    .fetch_one(pool)
     .await?;
 
-    Ok(rank.rank)
+    Ok(data.rank)
 }
 
-pub async fn get_user_row_number<T: TryInto<i64>>(user_id: T) -> Result<Option<i64>> {
-    let pool = sqlx_lib::get_pool().await?;
-
-    let user_id: i64 = user_id.try_into().map_err(|_| Error::ConversionError)?;
-
-    let row_number = sqlx::query!(
+pub async fn get_user_row_number(
+    pool: &Pool<Postgres>,
+    user_id: impl TryInto<i64>,
+) -> Result<Option<i64>> {
+    let data = sqlx::query!(
         "SELECT row_number FROM (SELECT id, ROW_NUMBER() OVER (ORDER BY total_xp DESC) FROM levels) AS ranked WHERE id = $1",
-        user_id
+        user_id.try_into().map_err(|_| Error::ConversionError)?
     )
-    .fetch_one(&pool)
+    .fetch_one(pool)
     .await?;
 
-    Ok(row_number.row_number)
+    Ok(data.row_number)
 }
 
-pub async fn get_users<T: Into<i64>>(page: T) -> Result<Vec<UserLevelData>> {
-    let pool = sqlx_lib::get_pool().await?;
-
-    let offset = (page.into() - 1) * LIMIT;
+pub async fn get_users(
+    pool: &Pool<Postgres>,
+    page: impl Into<i64>,
+    limit: i64,
+) -> Result<Vec<UserLevelData>> {
+    let offset = (page.into() - 1) * limit;
 
     let data = sqlx::query_as!(
         UserLevelData,
         "SELECT * FROM levels ORDER BY total_xp DESC LIMIT $1 OFFSET $2",
-        LIMIT,
+        limit,
         offset
     )
-    .fetch_all(&pool)
+    .fetch_all(pool)
     .await?;
 
     Ok(data)
