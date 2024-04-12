339ae5c16edf2bb36072375afcd57a7825b175b4
Oscar Six <26691547+KiloOscarSix@users.noreply.github.com>
2024-04-12 17:34:33 +0100

    Refactor cron.rs to use TEAM_LEADS_CHANNEL_ID and availability_check_message

--- src/cron.rs
+++ src/cron.rs
@@ -1,15 +1,12 @@
 use cron::Schedule;
-use serenity::all::{
-    ButtonStyle, ChannelId, Context, CreateButton, CreateEmbed, CreateMessage, Mentionable, RoleId,
-};
+use serenity::all::Context;
 use std::str::FromStr;
 use std::time::Duration;
 use tokio::time::sleep;
 
+use crate::guild_commands::slash_commands::availability_check::availability_check_message;
+use crate::guilds::college_kings_team::TEAM_LEADS_CHANNEL_ID;
 use crate::Result;
-
-const CHANNEL_ID: ChannelId = ChannelId::new(846021706203136030);
-const ROLE_ID: RoleId = RoleId::new(836275726352646176);
 
 pub async fn start_cron_jobs(ctx: Context) -> Result<()> {
     let mut jobs: Vec<(Schedule, _)> = vec![(
@@ -45,27 +42,10 @@ pub async fn start_cron_jobs(ctx: Context) -> Result<()> {
 async fn send_availability_check(ctx: Context) -> Result<()> {
     println!("Sending availability check");
 
-    CHANNEL_ID
+    TEAM_LEADS_CHANNEL_ID
         .send_message(
             &ctx,
-            CreateMessage::default()
-                .content(ROLE_ID.mention().to_string())
-                .embed(
-                    CreateEmbed::default()
-                        .title("Are you available for tomorrow's meeting?")
-                        .field("Attending", "", true)
-                        .field("Unavailable", "", true),
-                )
-                .button(
-                    CreateButton::new("cron_available")
-                        .label("Attending")
-                        .style(ButtonStyle::Success),
-                )
-                .button(
-                    CreateButton::new("cron_unavailable")
-                        .label("Unavailable")
-                        .style(ButtonStyle::Danger),
-                ),
+            availability_check_message("Are you available for tomorrow's meeting?"),
         )
         .await?;
 
