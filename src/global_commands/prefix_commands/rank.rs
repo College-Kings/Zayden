da1b62e7d6fb802ee4c4f5c830ac5e686b5d2b94
Oscar Six <26691547+KiloOscarSix@users.noreply.github.com>
2024-04-12 17:30:20 +0100

    Refactor ping.rs and rank.rs to pass Context as a reference in prefix_commands

--- src/global_commands/prefix_commands/rank.rs
+++ src/global_commands/prefix_commands/rank.rs
@@ -2,7 +2,7 @@ use serenity::all::{Context, Message};
 
 use crate::Result;
 
-pub async fn run(ctx: Context, msg: Message) -> Result<()> {
+pub async fn run(ctx: &Context, msg: Message) -> Result<()> {
     msg.channel_id
         .say(
             &ctx,
