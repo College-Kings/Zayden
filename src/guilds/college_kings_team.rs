b1877588d7a84cccd041a52031f5fe795e40e268
Oscar Six <26691547+KiloOscarSix@users.noreply.github.com>
2024-04-12 17:32:19 +0100

    Update guild constants in college_kings_team.rs

--- src/guilds/college_kings_team.rs
+++ src/guilds/college_kings_team.rs
@@ -1,3 +1,5 @@
-use serenity::all::GuildId;
+use serenity::all::{ChannelId, GuildId, RoleId};
 
 pub const GUILD_ID: GuildId = GuildId::new(814314852855447634);
+pub const TEAM_LEADS_CHANNEL_ID: ChannelId = ChannelId::new(846021706203136030);
+pub const TEAM_LEADS_ROLE_ID: RoleId = RoleId::new(836275726352646176);
