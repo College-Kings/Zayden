3920bdfc1127f28c09473ac92f2a0158b5ca5b67
Oscar Six <26691547+KiloOscarSix@users.noreply.github.com>
2024-04-13 11:53:37 +0100

    Refactor ready function to accept Context as a reference in ready/mod.rs

--- src/handler/ready/mod.rs
+++ src/handler/ready/mod.rs
@@ -7,21 +7,21 @@ use serenity::all::{Context, OnlineStatus, Ready};
 use crate::cron::start_cron_jobs;
 use crate::{global_commands, guild_commands, Result};
 
-pub async fn ready(ctx: Context, ready: Ready) -> Result<()> {
+pub async fn ready(ctx: &Context, ready: Ready) -> Result<()> {
     println!("{} is connected!", ready.user.name);
 
     ctx.set_presence(None, OnlineStatus::Online);
 
     // TODO: Load Commands
 
+    tokio::try_join!(
+        guild_commands::register(ctx),
+        global_commands::register(ctx),
+        update_messages(ctx),
+    )?;
+
     let ctx_clone = ctx.clone();
     tokio::spawn(async move { start_cron_jobs(ctx_clone).await }).await??;
-
-    tokio::try_join!(
-        guild_commands::register(&ctx),
-        global_commands::register(&ctx),
-        update_messages(&ctx),
-    )?;
 
     Ok(())
 }
