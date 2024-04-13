18c48ec7c684deb839450833349313dd893b539c
Oscar Six <26691547+KiloOscarSix@users.noreply.github.com>
2024-04-13 11:53:26 +0100

    Refactor reaction handler functions to accept Context as a reference in reaction/mod.rs

--- src/handler/reaction/mod.rs
+++ src/handler/reaction/mod.rs
@@ -3,26 +3,26 @@ mod suggestions;
 
 use serenity::all::{Context, Reaction};
 
-use crate::{college_kings::SUGGESTION_CATEGORY_ID, Result};
+use crate::{guilds::college_kings::SUGGESTION_CATEGORY_ID, Result};
 
-pub async fn reaction_add(ctx: Context, reaction: Reaction) -> Result<()> {
-    reaction_roles::reaction_add(&ctx, &reaction).await?;
+pub async fn reaction_add(ctx: &Context, reaction: Reaction) -> Result<()> {
+    reaction_roles::reaction_add(ctx, &reaction).await?;
 
     if let Some(channel) = reaction.channel(&ctx).await?.guild() {
         if channel.parent_id == Some(SUGGESTION_CATEGORY_ID) {
-            suggestions::suggestion(&ctx, &reaction, channel).await?;
+            suggestions::suggestion(ctx, &reaction, channel).await?;
         }
     }
 
     Ok(())
 }
 
-pub async fn reaction_remove(ctx: Context, reaction: Reaction) -> Result<()> {
-    reaction_roles::reaction_remove(&ctx, &reaction).await?;
+pub async fn reaction_remove(ctx: &Context, reaction: Reaction) -> Result<()> {
+    reaction_roles::reaction_remove(ctx, &reaction).await?;
 
     if let Some(channel) = reaction.channel(&ctx).await?.guild() {
         if channel.parent_id == Some(SUGGESTION_CATEGORY_ID) {
-            suggestions::suggestion(&ctx, &reaction, channel).await?;
+            suggestions::suggestion(ctx, &reaction, channel).await?;
         }
     }
 
