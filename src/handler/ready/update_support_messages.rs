aa55367a7ff0a0b91ee5064062ce5f766c1000d7
Oscar Six <26691547+KiloOscarSix@users.noreply.github.com>
2024-04-14 16:50:56 +0100

    Refactor update_support_messages.rs to remove debug print statement

--- src/handler/ready/update_support_messages.rs
+++ src/handler/ready/update_support_messages.rs
@@ -9,7 +9,6 @@ const CHANNEL_ID: ChannelId = ChannelId::new(919950775134847016);
 pub async fn run(ctx: &Context) -> Result<()> {
     let mut messages = CHANNEL_ID.messages_iter(&ctx).boxed();
     while let Some(Ok(message)) = messages.next().await {
-        println!("{:?}", message);
         if let Some(ActionRowComponent::Button(b)) = message
             .components
             .first()
