ce07f232e0e705de6457e0684d99490050d9b08d
Oscar Six <26691547+KiloOscarSix@users.noreply.github.com>
2024-04-19 03:38:53 +0100

    Add GENERAL_CHANNEL_ID constant to college_kings.rs

--- src/guilds/college_kings.rs
+++ src/guilds/college_kings.rs
@@ -1,4 +1,5 @@
 use serenity::all::{ChannelId, GuildId};
 
 pub const GUILD_ID: GuildId = GuildId::new(745662812335898806);
+pub const GENERAL_CHANNEL_ID: ChannelId = ChannelId::new(745662813036609548);
 pub const SUGGESTION_CATEGORY_ID: ChannelId = ChannelId::new(1068790374996377671);
