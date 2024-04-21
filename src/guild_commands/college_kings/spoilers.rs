bea1cdfe7dd5e335ac629f2ba055f26942a88cc2
Oscar Six <26691547+KiloOscarSix@users.noreply.github.com>
2024-04-21 22:25:03 +0100

    Refactor spoiler command to use channel mentions and handle missing channels

--- src/guild_commands/college_kings/spoilers.rs
+++ src/guild_commands/college_kings/spoilers.rs
@@ -1,7 +1,9 @@
 use crate::sqlx_lib::{get_pool, get_spoiler_channel_ids, get_support_channel_ids};
 use crate::utils::message_response;
 use crate::{Error, Result};
-use serenity::all::{CommandInteraction, Context, CreateCommand};
+use serenity::all::{
+    ChannelId, CommandInteraction, Context, CreateCommand, GuildChannel, GuildId, Mentionable,
+};
 
 pub async fn run(ctx: &Context, interaction: &CommandInteraction) -> Result<()> {
     interaction.defer(ctx).await?;
@@ -10,25 +12,35 @@ pub async fn run(ctx: &Context, interaction: &CommandInteraction) -> Result<()> 
 
     let pool = get_pool(ctx).await?;
 
+    let no_channel = ChannelId::new(1);
+
     let support_thread_ids = get_support_channel_ids(&pool, guild_id.get()).await?;
-    let support_thread_id = support_thread_ids
-        .first()
-        .ok_or_else(|| Error::NoSupportThread)?;
+    let support_thread_id = support_thread_ids.first().unwrap_or(&no_channel);
 
     let spoiler_thread_ids = get_spoiler_channel_ids(&pool, guild_id.get()).await?;
-    let spoiler_thread_id = spoiler_thread_ids
-        .first()
-        .ok_or_else(|| Error::NoSpoilerThread)?;
+    let spoiler_thread_id = match spoiler_thread_ids.first() {
+        Some(id) => *id,
+        None => get_spoiler_channel_by_name(ctx, guild_id).await?.id,
+    };
 
     message_response(ctx, interaction, &format!(
 "Spoilers are defined as any content that has not been released on all supported platforms for at least 2 weeks.
-Please keep all conversations about spoilers to <#{}>
-If you have any bugs or questions please post them in <#{}>",
-spoiler_thread_id, support_thread_id)).await?;
+Please keep all conversations about spoilers to {}.
+If you have any bugs or questions please post them in {}",
+spoiler_thread_id.mention(), support_thread_id.mention())).await?;
 
     Ok(())
 }
 
 pub fn register() -> CreateCommand {
     CreateCommand::new("spoilers").description("Disclaimer about spoilers")
+}
+
+async fn get_spoiler_channel_by_name(ctx: &Context, guild_id: GuildId) -> Result<GuildChannel> {
+    guild_id
+        .channels(&ctx)
+        .await?
+        .into_values()
+        .find(|c| c.name.contains("spoilers"))
+        .ok_or_else(|| Error::NoChannel)
 }
