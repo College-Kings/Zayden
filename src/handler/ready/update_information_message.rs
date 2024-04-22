98863eeb1adcb40dfcbe1925d7a4517bf673e374
Oscar Six <26691547+KiloOscarSix@users.noreply.github.com>
2024-04-22 04:47:35 +0100

    Update information channels and discussion channels in update_information_message.rs

--- src/handler/ready/update_information_message.rs
+++ src/handler/ready/update_information_message.rs
@@ -15,26 +15,23 @@ Get the latest version here:
 [College Kings 2](https://store.steampowered.com/app/1924480?utm_source=ck_discord_information&utm_medium=link&utm_term=ck2chapter1)
 Get the latest patreon version here:
 https://patreon.com/collegekings"#)
-    .field("Information Channels", r"<#830927865784565800> ~ Welcome!
-<#747430712617074718> ~ Make sure you are fully familiar with the rules. Ignorance is not a defence.
-<#747426032453156924> ~ General announcement about the game or the discord server
-<#797859157562359888> ~ All discord server events are announced here
-<#803728389500174378> ~ Public patreon posts are announced here.
-<#885713583948836874> ~ Get to know the team behind College Kings and the discord server
-<#806504327307853826> ~ New patrons will be announced here, thanks for the support!
+    .field("Information Channels", r"<#830927865784565800> ~ You're here!
+<#747430712617074718> ~ Please make sure you are fully familiar with the rules.
+<#747426032453156924> ~ Announcements about the game or the discord server.
+<#803728389500174378> ~ Patreon link and information.
+<#885713583948836874> ~ Get to know the team behind College Kings.
 <#867385605553389618> ~ A live development log to see what's happening
 <#805765564504473641> ~ You can get your custom roles here, from favourite characters club to event announcements.", false)
-    .field("Discussion Channels", r"<#745662813036609548> ~ Highly moderated and regulated chat
-<#787774961850646559> ~ [NSFW] Feel free to discuss anything provided it doesn't break the rules
+.field("Discussion Channels", "<#745662813036609548> ~ Primarily place for discussing College Kings and related topics.
+<#787774961850646559> ~ [NSFW] Chat about anything else here. NSFW content is allowed, but no pornography.
 <#1126255347309486110> ~ [NSFW] Post your favourite pictures or videos here. NSFW media has to be 2d/3d, no NSFW real life imagery. Message <@600056695586291762> if you're unsure
 <#880870369198751764> ~ Discussions about Sports usually happen here
-<#832695012709892107> ~ 54 61 6c 6b 20 61 62 6f 75 74 20 63 6f 64 65 20 77 69 74 68 20 74 68 65 20 43 6f 64 65 20 4d 6f 6e 6b 65 79 73 2e
-<#747428461391380532> ~ [SPOILERS] Theories about the game including content from the latest updates
 <#770621445637799946> ~ [SPOILERS] Here you can discuss College Kings without having to spoiler mark you messages
+<#747428461391380532> ~ [SPOILERS] Theories about the game including content from the latest updates
 <#817235460467720194> ~ [SPOILERS] Like to rank the ck characters? Let the world know about your ranking here
 <#1020000314260205670> ~ [NSFW] Discussion around games that aren't College Kings
 <#776139754408247326> ~ Please only use bot commands in this channel!", false)
-    .field("Support Channels", r"<#1068790374996377671> ~ Find community suggestions here
+.field("Support Channels", "<#1068790374996377671> ~ Find community suggestions here
 <#829463308629180447> ~ You want to ask a question and are not sure if someone else already asked? Take a look in here
 <#919950775134847016> ~ Do you need help or did you find any bugs? Make sure to ask about it here.", false)
     .field("Roles", r"**Staff Roles:**
