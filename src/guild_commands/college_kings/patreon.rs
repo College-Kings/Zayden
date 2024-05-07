0cbeaced39e33b09788cd57e0f59528ab2b6ca3d
Oscar Six <26691547+KiloOscarSix@users.noreply.github.com>
2024-05-07 10:57:54 +0100

    Add patreon login function

--- src/guild_commands/college_kings/patreon.rs
+++ src/guild_commands/college_kings/patreon.rs
@@ -1,10 +1,16 @@
+use std::time::Duration;
+
+use reqwest::Client;
 use serenity::all::{
     CommandInteraction, CommandOptionType, Context, CreateCommand, CreateCommandOption,
     CreateEmbed, CreateEmbedFooter, ResolvedValue,
 };
+use url::Url;
 
-use crate::utils::{embed_response, parse_options};
-use crate::{patreon_lib, Result};
+use crate::utils::{embed_response, message_response, parse_options};
+use crate::{patreon_lib, Result, SERVER_URL};
+
+const CLIENT_ID: &str = "co3TJ3lwqHN5WSVuIBiDNhfQv28V4FR-z6g-_fIogDzj_Um09DoWLGE5rvAJeTQd";
 
 async fn info(ctx: &Context, interaction: &CommandInteraction) -> Result<()> {
     interaction.defer(ctx).await?;
@@ -16,6 +22,42 @@ async fn info(ctx: &Context, interaction: &CommandInteraction) -> Result<()> {
             .thumbnail("https://images-ext-2.discordapp.net/external/QOCCliX2PNqo717REOwxtbvIrxVV2DZ1CRc8Svz3vUs/https/collegekingsgame.com/wp-content/uploads/2020/08/college-kings-wide-white.png")
             .footer(CreateEmbedFooter::new("https://www.patreon.com/collegekings"))
     ).await?;
+
+    Ok(())
+}
+
+async fn login(ctx: &Context, interaction: &CommandInteraction) -> Result<()> {
+    interaction.defer_ephemeral(ctx).await?;
+
+    let patreon_url = Url::parse(&format!("https://www.patreon.com/oauth2/authorize?response_type=code&client_id={CLIENT_ID}&redirect_uri={SERVER_URL}/api/v1/patreon/oauth/zayden&state={}", interaction.user.id)).unwrap();
+    let url_as_str = patreon_url.as_str();
+
+    message_response(ctx, interaction, url_as_str).await?;
+
+    let client = Client::new();
+
+    tokio::time::sleep(Duration::from_secs(5)).await;
+
+    for _ in 0..10 * 60 {
+        if patreon_lib::get_user(&client, interaction.user.id, false)
+            .await
+            .is_ok()
+        {
+            message_response(ctx, interaction, "Status: Success!").await?;
+
+            return Ok(());
+        } else {
+            message_response(
+                ctx,
+                interaction,
+                format!("{url_as_str}\n\nStatus: User not currently in cache."),
+            )
+            .await?;
+        }
+        tokio::time::sleep(Duration::from_secs(1)).await;
+    }
+
+    message_response(ctx, interaction, "Status: Timeout").await?;
 
     Ok(())
 }
@@ -44,7 +86,7 @@ async fn check(
         _ => false,
     };
 
-    let attributes = patreon_lib::get_user(email, force).await?;
+    let attributes = patreon_lib::get_user(&Client::new(), email, force).await?;
 
     embed_response(
         ctx,
@@ -72,6 +114,7 @@ pub async fn run(ctx: &Context, interaction: &CommandInteraction) -> Result<()> 
 
     match command.name {
         "info" => info(ctx, interaction).await?,
+        "login" => login(ctx, interaction).await?,
         "check" => check(ctx, interaction, &command.value).await?,
         _ => unreachable!("Unknown subcommand"),
     };
@@ -86,6 +129,11 @@ pub fn register() -> CreateCommand {
             CommandOptionType::SubCommand,
             "info",
             "Patreon information",
+        ))
+        .add_option(CreateCommandOption::new(
+            CommandOptionType::SubCommand,
+            "login",
+            "Login to Patreon",
         ))
         .add_option(
             CreateCommandOption::new(
