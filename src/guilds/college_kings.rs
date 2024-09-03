b2b3f1d402e5e111b723f31c54c1706ebbaaaef9
Oscar Six <26691547+KiloOscarSix@users.noreply.github.com>
2024-09-03 02:34:33 +0100

    Refactor guild commands and add new commands for College Kings guilds

--- src/guilds/college_kings.rs
+++ src/guilds/college_kings.rs
@@ -1,16 +1,33 @@
-use serenity::all::{ChannelId, GuildId, RoleId};
+use serenity::all::{ChannelId, CreateCommand, GuildId};
+
+use crate::guild_commands::college_kings::*;
 
 pub const GUILD_ID: GuildId = GuildId::new(745662812335898806);
 
 pub const SUPPORT_FAQ_CHANNEL_ID: ChannelId = ChannelId::new(1224557981849485392);
 
 pub const INFORMATION_CHANNEL_ID: ChannelId = ChannelId::new(830927865784565800);
-pub const RULE_CHANNEL_ID: ChannelId = ChannelId::new(747430712617074718);
 pub const CHANGE_LOG_CHANNEL_ID: ChannelId = ChannelId::new(992599169288122410);
-pub const GENERAL_CHANNEL_ID: ChannelId = ChannelId::new(745662813036609548);
 pub const RENDER_REQUESTS_CHANNEL_ID: ChannelId = ChannelId::new(1235269134246346816);
 pub const SUGGESTION_CATEGORY_ID: ChannelId = ChannelId::new(1068790374996377671);
 pub const FAQ_CHANNEL_ID: ChannelId = ChannelId::new(1196346920059289690);
-pub const SUPPORT_CHANNEL_ID: ChannelId = ChannelId::new(919950775134847016);
 
-pub const ARTIST_ROLE_ID: RoleId = RoleId::new(1043987303556726854);
+pub fn commands() -> Vec<CreateCommand> {
+    vec![
+        add_artist::register(),
+        availability_check::register(),
+        close::register(),
+        faq::register(),
+        fetch_suggestions::register(),
+        fixed::register(),
+        get_discord_role::register(),
+        goodmorning::register(),
+        goodnight::register(),
+        image::register(),
+        open::register(),
+        reputation::register(),
+        saves::register(),
+        spoilers::register(),
+        support::register(),
+    ]
+}
--- src/guilds/college_kings_team.rs
+++ src/guilds/college_kings_team.rs
@@ -1,12 +1,17 @@
-use serenity::all::{ChannelId, GuildId, RoleId, UserId};
+use serenity::all::{ChannelId, CreateCommand, GuildId, RoleId, UserId};
+
+use crate::guild_commands::college_kings_team::*;
 
 pub const GUILD_ID: GuildId = GuildId::new(814314852855447634);
 
 pub const TEAM_LEADS_CHANNEL_ID: ChannelId = ChannelId::new(846021706203136030);
 pub const REVIEW_CHANNEL_ID: ChannelId = ChannelId::new(1227244734515380316);
-pub const SUGGESTION_CHANNEL_ID: ChannelId = ChannelId::new(1225381390539821086);
 
 pub const TEAM_LEADERS_ROLE_ID: RoleId = RoleId::new(836275726352646176);
 
 pub const STEVE_USER_ID: UserId = UserId::new(747423760780623872);
 pub const MESSY_USER_ID: UserId = UserId::new(841466088612298793);
+
+pub fn commands() -> Vec<CreateCommand> {
+    vec![review::register()]
+}
